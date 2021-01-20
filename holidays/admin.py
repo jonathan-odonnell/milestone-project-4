62dfb7f7ee0f4d2c6271491f9a4c420983f5e14a
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-01-20 20:28:59 +0000

    Customised admin

--- holidays/admin.py
+++ holidays/admin.py
@@ -2,8 +2,42 @@ from django.contrib import admin
 from .models import Activity, Category, Country, Feature, Flight, Itinerary, Price, Package
 
 
+class ActivityInline(admin.StackedInline):
+    model = Package.activities.through
+    extra = 0
+    verbose_name = 'Activity'
+    verbose_name_plural = 'Activities'
+
+
+class FeatureInline(admin.StackedInline):
+    model = Package.features.through
+    extra = 0
+    verbose_name = 'Feature'
+    verbose_name_plural = 'Features'
+
+
+class ItineraryInline(admin.StackedInline):
+    model = Itinerary
+    extra = 0
+
+
+class PriceInline(admin.StackedInline):
+    model = Price
+    extra = 0
+
+
+class PackageAdmin(admin.ModelAdmin):
+    inlines = [
+        PriceInline,
+        FeatureInline,
+        ActivityInline,
+        ItineraryInline,
+    ]
+    exclude = ('features', 'activities', 'itinerary',)
+
+
 admin.site.register(Category)
 admin.site.register(Country)
 admin.site.register(Feature)
 admin.site.register(Flight)
-admin.site.register(Package)
+admin.site.register(Package, PackageAdmin)
