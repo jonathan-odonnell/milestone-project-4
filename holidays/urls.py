5979a076b6f781bb8aaae02700066494b94204b2
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-01-24 16:57:15 +0000

    Amended holiday details url

--- holidays/urls.py
+++ holidays/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('<str:category>/', views.category_holidays, name='categories'),
     path('destinations/<str:destination>/', views.destination_holidays, name='destinations'),
-    path('details/<str:slug>/', views.holiday_details, name='holiday_details'),
+    path('details/<slug:slug>/', views.holiday_details, name='holiday_details'),
 ]
