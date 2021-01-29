a7aa9045a6bb5d35fe637ab35ed96a9602ad1fc2
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-01-29 18:37:32 +0000

    Added add booking url and view

--- booking/urls.py
+++ booking/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.booking, name='booking'),
+    path('<int:holiday_id>', views.add_booking, name='add_booking')
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,4 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.views.decorators.http import require_POST
 
 def booking(request):
-    return render(request, 'booking/booking.html')    
+    return render(request, 'booking/booking.html') 
+
+@require_POST
+def add_booking(request, holiday_id):
+    guests = int(request.POST.get('guests'))
+    booking = request.session.get('booking', {})
+    booking['holiday_id'] = holiday_id
+    booking['guests'] = guests
+    booking['departure_airport'] = request.POST.get('departure_airport')
+    booking['departure_date'] = request.POST.get('departure_date')
+    request.session['booking'] = booking
+
+    return redirect(reverse('booking'))
