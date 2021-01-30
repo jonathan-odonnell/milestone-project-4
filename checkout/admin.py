fb2dca2be1a2efbae2e95f890fae5d9f930c24c6
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-01-30 17:13:32 +0000

    Added checkout forms, admin and signals

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Booking, PackageBooking
 
-# Register your models here.
+
+class PackageBookingAdminInline(admin.TabularInline):
+    model = PackageBooking
+    readonly_fields = ('package_total',)
+
+
+class BookingAdmin(admin.ModelAdmin):
+    inlines = (PackageBookingAdminInline,)
+
+    readonly_fields = ('booking_number', 'date', 'total',)
+
+    fields = ('booking_number', 'date', 'full_name',
+              'email', 'phone_number', 'country',
+              'postcode', 'town_or_city', 'street_address1',
+              'street_address2', 'county', 'total',)
+
+    list_display = ('booking_number', 'date', 'full_name',
+                    'total',)
+
+    ordering = ('-date',)
+
+admin.site.register(Booking, BookingAdmin)
