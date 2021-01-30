db1838073a7b29ba94e055ecbc25fb5a7d7c5c34
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-01-30 14:06:08 +0000

    Updated contexts processor total calculation

--- booking/contexts.py
+++ booking/contexts.py
@@ -12,7 +12,8 @@ def booking_details(request):
     holiday = None
     flights = []
     extras = 0
-    discount = booking.get('discount', 0)
+    coupon = None
+    discount = 0
     subtotal = 0
     total = 0
 
@@ -20,15 +21,22 @@ def booking_details(request):
         holiday = get_object_or_404(Package, pk=booking['holiday_id'], price__start_date__lte=date, price__end_date__gte=date)
         booking['departure_date'] = datetime.datetime.strptime(booking['departure_date'], "%Y-%m-%d").date()
         booking['return_date'] = booking['departure_date'] + datetime.timedelta(days=int(holiday.duration))
-        subtotal = Decimal(holiday.price_set.all()[0].price * booking['guests'])
-        total = subtotal + extras - discount
         flights.append(Flight.objects.all().filter(outbound_flight__pk=booking['holiday_id'], origin=booking['departure_airport']))
         flights.append(Flight.objects.all().filter(inbound_flight__pk=booking['holiday_id'], destination=booking['departure_airport']))
+
+        if booking.get('discount'):
+            coupon = booking.get('coupon')
+            discount = Decimal(booking.get('discount'))
+
+        subtotal = Decimal(holiday.price_set.all()[0].price * booking['guests'])
+        total = Decimal(subtotal + extras - discount)
 
     context = {
         'booking': booking,
         'holiday': holiday,
         'flights': flights,
+        'coupon': coupon,
+        'discount': discount,
         'subtotal': subtotal,
         'total': total
     }
