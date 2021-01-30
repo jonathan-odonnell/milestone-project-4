6db8aeaf9a5cf462a0b3d7907252b7853d3793fa
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-01-30 13:58:36 +0000

    Added add coupon view and url

--- booking/urls.py
+++ booking/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.booking, name='booking'),
     path('<int:holiday_id>', views.add_booking, name='add_booking'),
     path('update_guests/', views.update_guests, name='update_guests'),
+    path('add_coupon/', views.add_coupon, name='add_coupon'),
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, redirect, reverse
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
+from django.core.exceptions import ObjectDoesNotExist
 from holidays.models import Package
+from .models import Coupon
 from .contexts import booking_details
 import datetime
 from decimal import Decimal
 
+
 def booking(request):
-    return render(request, 'booking/booking.html') 
+    return render(request, 'booking/booking.html')
+
 
 @require_POST
 def add_booking(request, holiday_id):
@@ -21,18 +25,59 @@ def add_booking(request, holiday_id):
 
     return redirect(reverse('booking'))
 
+
 @require_POST
 def update_guests(request):
     booking = request.session.get('booking')
     guests = int(request.POST.get('guests'))
     date = datetime.datetime.now()
-    holiday = Package.objects.get(pk=booking['holiday_id'], price__start_date__lte=date, price__end_date__gte=date)
+    holiday = Package.objects.get(
+        pk=booking['holiday_id'], price__start_date__lte=date, price__end_date__gte=date)
     booking['guests'] = guests
     request.session['booking'] = booking
+    discount = Decimal(booking['discount'])
+    coupon = booking['coupon']
     subtotal = Decimal(holiday.price_set.all()[0].price * booking['guests'])
-    total = subtotal
+    total = Decimal(subtotal - discount)
 
-    return JsonResponse({'success': True, 'subtotal': subtotal, 'total': total})
+    response = {
+            'success': True,
+            'subtotal': subtotal,
+            'total': total,
+            'discount': discount,
+            'coupon': coupon
+        }
+
+    return JsonResponse(response)
 
 
+@require_POST
+def add_coupon(request):
+    try:
+        coupon_name = request.POST.get('coupon')
+        date = datetime.datetime.now()
+        discount = Coupon.objects.get(name__iexact=coupon_name, start_date__lte=date, end_date__gte=date)
+        booking = request.session.get('booking')
+        holiday = Package.objects.get(
+            pk=booking['holiday_id'],
+            price__start_date__lte=date,
+            price__end_date__gte=date)
+        booking['coupon'] = coupon_name
+        booking['discount'] = str(discount.amount)
+        request.session['booking'] = booking
+        subtotal = Decimal(holiday.price_set.all()[
+                           0].price * booking['guests'])
+        total = Decimal(subtotal - discount.amount)
 
+        response = {
+            'success': True,
+            'subtotal': subtotal,
+            'total': total,
+            'discount': discount.amount,
+            'coupon': coupon_name
+        }
+
+        return JsonResponse(response)
+
+    except ObjectDoesNotExist:
+        return JsonResponse({'success': False})
