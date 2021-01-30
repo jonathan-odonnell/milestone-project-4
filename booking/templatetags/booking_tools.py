5fd742639f7a749a86dcd2bd199f7de1ff52beba
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-01-30 14:33:38 +0000

    Added duration and extra days template tags to booking template

---
+++ booking/templatetags/booking_tools.py
@@ -0,0 +1,25 @@
+from django import template
+
+
+register = template.Library()
+
+
+@register.filter(name='duration')
+def duration(td):
+    #https://stackoverflow.com/questions/33105457/display-and-format-django-durationfield-in-template
+    total_seconds = int(td.total_seconds())
+    hours = total_seconds // 3600
+    minutes = (total_seconds % 3600) // 60
+
+    return '{}h {}m'.format(hours, minutes)
+
+
+@register.filter(name='next_day')
+def next_day(flight):
+    departure = flight.departure_time.date()
+    arrival = flight.arrival_time.date()
+    difference = arrival - departure
+
+    return difference.days
+
+    
