a7329b6f565063f57430c20a07e8bdc591099842
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-01-30 13:53:29 +0000

    Added coupons model

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Coupon(models.Model):
+    
+    name = models.CharField(max_length=254)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    amount = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return "{} {} - {}".format(self.name, self.start_date, self.end_date)
