f87fe61d631000e58ee4f8dba7677b1d7dd1e346
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-02-01 21:35:46 +0000

    Amended booking model and admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -12,14 +12,16 @@ class BookingAdmin(admin.ModelAdmin):
 
     readonly_fields = ('booking_number', 'date', 'total', 'stripe_pid')
 
-    fields = ('booking_number', 'date', 'full_name',
-              'email', 'phone_number', 'country',
-              'postcode', 'town_or_city', 'street_address1',
-              'street_address2', 'county', 'total', 'stripe_pid')
+    fields = ('booking_number', 'user_profile', 'date', 
+              'full_name', 'email', 'phone_number',
+              'street_address1', 'street_address2',
+              'town_or_city',  'country', 'postcode', 
+              'county', 'total', 'stripe_pid')
 
     list_display = ('booking_number', 'date', 'full_name',
                     'total',)
 
     ordering = ('-date',)
+
 
 admin.site.register(Booking, BookingAdmin)
--- checkout/models.py
+++ checkout/models.py
@@ -4,10 +4,13 @@ from django.db.models import Sum
 from django.conf import settings
 from holidays.models import Package
 from django_countries.fields import CountryField
+from profiles.models import UserProfile 
 
 class Booking(models.Model):
     booking_number = models.CharField(max_length=32, null=False, editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True, related_name='bookings')
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
