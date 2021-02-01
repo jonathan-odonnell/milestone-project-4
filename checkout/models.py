026e2be51e1b22ce2150cfa347c2678560bbec3b
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-02-01 19:46:14 +0000

    Updated checkout form

--- checkout/models.py
+++ checkout/models.py
@@ -3,18 +3,19 @@ from django.db import models
 from django.db.models import Sum
 from django.conf import settings
 from holidays.models import Package
+from django_countries.fields import CountryField
 
 class Booking(models.Model):
     booking_number = models.CharField(max_length=32, null=False, editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
-    country = models.CharField(max_length=40, null=False, blank=False)
-    postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
     county = models.CharField(max_length=80, null=True, blank=True)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     coupon = models.CharField(max_length=20, null=True, blank=True)
     total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
