4dd4b7b593cc2b18b9c933a406d142cd1cf3ff1e
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-02-07 23:30:26 +0000

    Added extras app and models

---
+++ extras/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django_extensions.db.fields import AutoSlugField
+
+
+def slugify(content):
+    """
+    A function to generate the package's slug. Code is from 
+    https://django-extensions.readthedocs.io/en/latest/field_extensions.html
+    """
+    return content.replace(' ', '-').lower()
+
+
+class Extra(models.Model):
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField()
+    image_url = models.CharField(max_length=254, null=True, blank=True)
+    slug = AutoSlugField(populate_from='name', slugify_function=slugify)
+
+    def __str__(self):
+        return f'{self.name} - £{self.price}'
