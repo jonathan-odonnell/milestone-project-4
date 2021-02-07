8d2f7ad43b0a92f46afd86d3d779d7ac8f5f5844
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-02-07 22:04:10 +0000

    Added bookings and booking details views and templates

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.profile, name='profile'),
+    path('bookings/', views.bookings, name='bookings'),
+    path('<booking_number>', views.booking_details, name='booking_details'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import UserProfile
+from checkout.models import Booking
 from .forms import UserProfileForm
 
 
@@ -11,22 +12,46 @@ def profile(request):
         form = UserProfileForm(request.POST, instance=profile)
         if form.is_valid():
             user_profile = form.save(commit=False)
-            user_profile.user.email=request.POST['email_address']
+            user_profile.user.email = request.POST['email_address']
             user_profile.save()
 
-    form = UserProfileForm(initial={'email_address': profile.user.email,
-                                    'phone_number': profile.phone_number,
-                                    'street_address1': profile.street_address1,
-                                    'street_address2': profile.street_address2,
-                                    'town_or_city': profile.town_or_city,
-                                    'county': profile.county,
-                                    'country': profile.country,
-                                    'postcode': profile.postcode,
-                                    })
+    form = UserProfileForm(
+        initial={'email_address': profile.user.email,
+                 'phone_number': profile.phone_number,
+                 'street_address1': profile.street_address1,
+                 'street_address2': profile.street_address2,
+                 'town_or_city': profile.town_or_city,
+                 'county': profile.county,
+                 'country': profile.country,
+                 'postcode': profile.postcode,
+                 })
 
     template = 'profiles/profile.html'
     context = {
         'form': form,
+    }
+
+    return render(request, template, context)
+
+def bookings(request):
+    """ Display the user's bookings. """
+    profile = get_object_or_404(UserProfile, user=request.user)
+    bookings = profile.bookings.all()
+
+    template = 'profiles/bookings.html'
+    context = {
+        'bookings': bookings,
+    }
+
+    return render(request, template, context)
+
+def booking_details(request, booking_number):
+    booking = get_object_or_404(Booking, booking_number=booking_number)
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'booking': booking,
+        'from_booking': True,
     }
 
     return render(request, template, context)
