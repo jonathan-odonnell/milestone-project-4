87fde1d6a56d5d696353ebec426920bd04c9736f
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-02-18 23:21:59 +0000

    Fixed an issue with the booking urls causing the wrong url to load

--- booking/urls.py
+++ booking/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.booking, name='booking'),
-    path('<holiday_id>/', views.add_booking, name='add_booking'),
     path('update_guests/', views.update_guests, name='update_guests'),
     path('add_coupon/', views.add_coupon, name='add_coupon'),
     path('add_extra/<extra_id>/', views.add_extra, name='add_extra'),
     path('update_extra/<extra_id>/', views.update_extra, name='update_extra'),
     path('remove_extra/<extra_id>/', views.remove_extra, name='remove_extra'),
+    path('<holiday_id>/', views.add_booking, name='add_booking'),
 ]
