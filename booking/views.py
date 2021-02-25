e16f1f9096af4da922785710fcd8ea1ac780c6de
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-02-25 15:51:52 +0000

    Amended passengers view

--- booking/views.py
+++ booking/views.py
@@ -4,6 +4,7 @@ from django.views.decorators.http import require_POST
 from django.core.exceptions import ObjectDoesNotExist
 from holidays.models import Package, Flight
 from extras.models import Extra
+from checkout.models import Booking, BookingExtra, BookingPackage, BookingPassenger
 from profiles.models import UserProfile
 from .models import Coupon
 from .contexts import booking_details
@@ -82,7 +83,7 @@ def add_extra(request, extra_id):
     extras = booking_totals['extras_total']
     subtotal = booking_totals['subtotal']
     total = booking_totals['total']
-    
+
     response = {
         'success': True,
         'extras': extras,
@@ -103,7 +104,7 @@ def update_extra(request, extra_id):
     extras = booking_totals['extras_total']
     subtotal = booking_totals['subtotal']
     total = booking_totals['total']
-    
+
     response = {
         'success': True,
         'extras': extras,
@@ -122,7 +123,7 @@ def remove_extra(request, extra_id):
     extras = booking_totals['extras_total']
     subtotal = booking_totals['subtotal']
     total = booking_totals['total']
-    
+
     response = {
         'success': True,
         'extras': extras,
@@ -138,7 +139,8 @@ def add_coupon(request):
     coupon_name = request.POST.get('coupon')
     try:
         current_date = datetime.datetime.now()
-        coupon = Coupon.objects.get(name__iexact=coupon_name, start_date__lte=current_date, end_date__gte=current_date)
+        coupon = Coupon.objects.get(
+            name__iexact=coupon_name, start_date__lte=current_date, end_date__gte=current_date)
         booking = request.session.get('booking')
         booking['coupon_id'] = coupon.pk
         booking['coupon'] = coupon_name
@@ -161,9 +163,87 @@ def add_coupon(request):
     except Coupon.DoesNotExist:
         return JsonResponse({'success': False, 'coupon': coupon_name})
 
+
 def passengers(request):
     if request.method == 'POST':
+        booking = None
+        current_booking = request.session.get('booking', {})
+        passenger_range = range(current_booking['guests'])
+
+        if current_booking.get('booking_number'):
+            booking_number = current_booking.get('booking_number')
+            booking = Booking.objects.get(booking_number=booking_number)
+            booking.booking_package.delete()
+            booking.booking_extras.all().delete()
+            booking.booking_passengers.all().delete()
+            booking.date = datetime.datetime.now()
+            booking.save()
+
+        else:
+            booking = Booking()
+            booking.save()
+
+        try:
+            holiday = Package.objects.get(id=current_booking['holiday_id'])
+            outbound_flight = Flight.objects.get(
+                pk=current_booking['outbound_flight'])
+            return_flight = Flight.objects.get(
+                pk=current_booking['return_flight'])
+            booking_package = BookingPackage(
+                booking=booking,
+                package=holiday,
+                guests=current_booking['guests'],
+                departure_date=datetime.datetime.strptime(
+                    current_booking['departure_date'], "%Y-%m-%d").date(),
+                outbound_flight=outbound_flight,
+                return_flight=return_flight,
+                duration=holiday.duration,
+            )
+            booking_package.save()
+
+            if request.user.is_authenticated:
+                profile = UserProfile.objects.get(user=request.user)
+                booking.user_profile = profile
+                booking.save()
+
+            if current_booking.get('coupon'):
+                booking.coupon = current_booking['coupon']
+                booking.save()
+
+            if current_booking.get('extras'):
+                for extra_id, extra_quantity in current_booking['extras'].items():
+                    try:
+                        extra = Extra.objects.get(id=extra_id)
+                        booking_extra = BookingExtra(
+                            booking=booking,
+                            extra=extra,
+                            quantity=extra_quantity,
+                        )
+                        booking_extra.save()
+                    
+                    except Extra.DoesNotExist:
+                        booking.delete()
+                        return redirect(reverse('booking'))
+
+            for passenger in passenger_range:
+                booking_passenger = BookingPassenger(
+                    booking=booking,
+                    full_name=request.POST[f'name{passenger}'],
+                    date_of_birth=datetime.datetime.strptime(
+                    request.POST[f'dob{passenger}'], "%Y-%m-%d").date(),
+                    passport_number=int(request.POST[f'passport{passenger}'])
+                ) 
+                booking_passenger.save()
+
+            current_booking['booking_number'] = booking.booking_number
+            request.session['booking'] = current_booking 
+
+        except Package.DoesNotExist:
+            booking.delete()
+            return redirect(reverse('booking'))
+
         return redirect(reverse('checkout'))
+    
     else:
         if request.session.get('booking'):
             profile = None
@@ -178,6 +258,6 @@ def passengers(request):
                 'profile': profile,
             }
             return render(request, 'booking/passengers.html', context)
-        
+
         else:
-            return HttpResponse(403)
+            return redirect(reverse('booking'))
