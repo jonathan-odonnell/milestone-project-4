2544c0bf58bbeb12327bc3a69bf4fa1fe0543fcd
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-02-28 21:21:42 +0000

    Added add, edit and delete holiday urls, views and templates

--- holidays/urls.py
+++ holidays/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path('destinations/<slug:destination>/', views.destination_holidays, name='destinations'),
     path('destinations/<slug:destination>/<slug:slug>/', views.holiday_details, name='destination_details'),
+    path('add/', views.add_holiday, name='add_holiday'),
+    path('edit/<slug:package>/', views.edit_holiday, name='edit_holiday'),
+    path('delete/<slug:package>/', views.delete_holiday, name='delete_holiday'),
     path('<str:category>/<slug:slug>/', views.holiday_details, name='category_details'),
     path('<slug:category>/', views.category_holidays, name='categories'),
 ]
--- holidays/views.py
+++ holidays/views.py
@@ -1,21 +1,26 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
 from .models import Package, Category, Country, Region
 from django.db.models import Min
 from django.db.models.functions import Lower
 from django.template.loader import render_to_string
 from django.http import JsonResponse
 from django.core.paginator import Paginator
+from .forms import PackageForm
+
 
 def category_holidays(request, category):
     """ A view to show all holidays in the category, including sorting and search queries """
     category = category.replace('-', ' ')
-    category=get_object_or_404(Category, name__iexact=category)
-    holidays = Package.objects.filter(category__name=category).annotate(min_price=Min('price__price'))
-    countries = holidays.values_list('country__name', flat=True).distinct().order_by('country__name')
+    category = get_object_or_404(Category, name__iexact=category)
+    holidays = Package.objects.filter(
+        category__name=category).annotate(min_price=Min('price__price'))
+    countries = holidays.values_list(
+        'country__name', flat=True).distinct().order_by('country__name')
     current_countries = None
     sort = None
     direction = None
-    
+
     if request.method == 'POST':
         if 'sort' in request.POST:
             sort = request.POST['sort']
@@ -25,21 +30,25 @@ def category_holidays(request, category):
                 direction = request.POST['direction']
                 if direction == 'desc':
                     sort = f'-{sort}'
-                    
+
                 holidays = holidays.order_by(sort)
 
         if 'country' in request.POST:
-            current_countries = request.POST['country'].replace('_', ' ').split(',')
-            holidays = holidays.annotate(lower_country=Lower('country__name')).filter(lower_country__in=current_countries)
-            current_countries = Country.objects.annotate(lower_name=Lower('name')).filter(lower_name__in=current_countries).values_list('name', flat=True)
+            current_countries = request.POST['country'].replace(
+                '_', ' ').split(',')
+            holidays = holidays.annotate(lower_country=Lower('country__name')).filter(
+                lower_country__in=current_countries)
+            current_countries = Country.objects.annotate(lower_name=Lower('name')).filter(
+                lower_name__in=current_countries).values_list('name', flat=True)
 
         # https://stackoverflow.com/questions/50879653/django-render-template-in-template-using-ajax
         holidays = Paginator(holidays, 12)
         page_number = request.POST['page']
         holidays = holidays.get_page(page_number)
-        html = render_to_string('holidays/includes/holidays.html', {'holidays': holidays, 'category': category})
+        html = render_to_string(
+            'holidays/includes/holidays.html', {'holidays': holidays, 'category': category})
         return JsonResponse({'holidays': html, 'pages': holidays.paginator.num_pages})
-    
+
     else:
         holidays = Paginator(holidays, 12)
         page_number = None
@@ -52,12 +61,15 @@ def category_holidays(request, category):
 
         return render(request, 'holidays/categories.html', context)
 
+
 def destination_holidays(request, destination):
     """ A view to show holidays in the destination, including sorting and search queries """
     destination = destination.replace('-', ' ')
     destination = get_object_or_404(Region, name__iexact=destination)
-    holidays = Package.objects.filter(country__region=destination).annotate(min_price=Min('price__price'))
-    categories = holidays.values_list('category__name', flat=True).distinct().order_by('category__name')
+    holidays = Package.objects.filter(
+        country__region=destination).annotate(min_price=Min('price__price'))
+    categories = holidays.values_list(
+        'category__name', flat=True).distinct().order_by('category__name')
     current_categories = None
     sort = None
     direction = None
@@ -75,15 +87,19 @@ def destination_holidays(request, destination):
             holidays = holidays.order_by(sort)
 
         if 'category' in request.POST:
-            current_categories = request.POST['category'].replace('_', ' ').split(',')
-            holidays = holidays.annotate(lower_category=Lower('category__name')).filter(lower_category__in=current_categories)
-            current_categories = Category.objects.annotate(lower_name=Lower('name')).filter(lower_name__in=current_categories).values_list('name', flat=True)
+            current_categories = request.POST['category'].replace(
+                '_', ' ').split(',')
+            holidays = holidays.annotate(lower_category=Lower('category__name')).filter(
+                lower_category__in=current_categories)
+            current_categories = Category.objects.annotate(lower_name=Lower('name')).filter(
+                lower_name__in=current_categories).values_list('name', flat=True)
 
         # https://stackoverflow.com/questions/50879653/django-render-template-in-template-using-ajax
         holidays = Paginator(holidays, 12)
         page_number = request.POST['page']
         holidays = holidays.get_page(page_number)
-        html = render_to_string('holidays/includes/holidays.html', {'holidays': holidays, 'destination': destination})
+        html = render_to_string('holidays/includes/holidays.html',
+                                {'holidays': holidays, 'destination': destination})
         return JsonResponse({'holidays': html, 'pages': holidays.paginator.num_pages})
 
     else:
@@ -98,20 +114,25 @@ def destination_holidays(request, destination):
 
         return render(request, 'holidays/destinations.html', context)
 
+
 def holiday_details(request, slug, destination=None, category=None):
     """ A view to show individual holiday details """
     holiday = get_object_or_404(Package.objects
-        .annotate(min_price=Min('price__price')), slug=slug)
-    
+                                .annotate(min_price=Min('price__price')), slug=slug)
+
     if category:
         category = category.replace('-', ' ')
-        related_holidays = Package.objects.exclude(name=holiday.name).annotate(lower_category=Lower('category__name'), min_price=Min('price__price'))
-        related_holidays = related_holidays.filter(lower_category=category).order_by('?')[:4]
+        related_holidays = Package.objects.exclude(name=holiday.name).annotate(
+            lower_category=Lower('category__name'), min_price=Min('price__price'))
+        related_holidays = related_holidays.filter(
+            lower_category=category).order_by('?')[:4]
 
     else:
         destination = destination.replace('-', ' ')
-        related_holidays = Package.objects.exclude(name=holiday.name).annotate(lower_region=Lower('country__region__name'), min_price=Min('price__price'))
-        related_holidays = related_holidays.filter(lower_region=destination).order_by('?')[:4]
+        related_holidays = Package.objects.exclude(name=holiday.name).annotate(
+            lower_region=Lower('country__region__name'), min_price=Min('price__price'))
+        related_holidays = related_holidays.filter(
+            lower_region=destination).order_by('?')[:4]
 
     context = {
         'holiday': holiday,
@@ -120,3 +141,53 @@ def holiday_details(request, slug, destination=None, category=None):
         'destination': destination
     }
     return render(request, 'holidays/holiday_details.html', context)
+
+
+def add_holiday(request):
+    if request.method == 'POST':
+        form = PackageForm(request.POST, request.FILES)
+        if form.is_valid():
+            holiday = form.save()
+            messages.success(request, 'Successfully added holiday!')
+            return redirect(reverse('destination_details', args=[holiday.slug]))
+        else:
+            messages.error(
+                request, 'Failed to add holiday. Please ensure the form is valid.')
+
+    else:
+        form = PackageForm()
+
+    template = 'holidays/add_holiday.html'
+    context = {
+        'form': form,
+    }
+    return render(request, template, context)
+
+
+def edit_holiday(request, package):
+    holiday = get_object_or_404(Package, slug=package)
+    if request.method == 'POST':
+        form = PackageForm(request.POST, request.FILES, instance=holiday)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated holiday!')
+            return redirect(reverse('destination_details', args=[holiday.slug]))
+        else:
+            messages.error(
+                request, 'Failed to update holiday. Please ensure the form is valid.')
+    else:
+        form = PackageForm(instance=holiday)
+
+    template = 'holidays/edit_holiday.html'
+    context = {
+        'form': form,
+        'holiday': holiday,
+    }
+
+    return render(request, template, context)
+
+def delete_holiday(request, package):
+    holiday = get_object_or_404(Package, slug=package)
+    holiday.delete()
+    messages.success(request, 'Holiday deleted!')
+    return redirect(reverse('destinations'))
