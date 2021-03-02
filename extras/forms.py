44c8c9d31a966c998605642fa70c3d783bfef334
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-03-02 18:30:56 +0000

    Added extras form

---
+++ extras/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from holidays.widgets import CustomClearableFileInput
+from .models import Extra
+from crispy_forms.helper import FormHelper
+
+
+class ExtraForm(forms.ModelForm):
+    class Meta:
+        model = Extra
+        exclude = ('image_url',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.field_class = 'mb-3'
+        self.helper.label_class = 'form-label'
+        self.fields['image'].widget = CustomClearableFileInput()
