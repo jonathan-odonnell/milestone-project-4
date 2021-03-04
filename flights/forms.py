eec09be60e5254201d4556631fa98d6183a60915
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-03-04 23:23:50 +0000

    Added flight form

---
+++ flights/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from holidays.models import Flight
+
+class FlightForm(forms.ModelForm):
+
+    class Meta:
+        model = Flight
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.field_class = 'mb-3'
+        self.helper.label_class = 'form-label'
+        self.fields['name'].widget.attrs['autofocus'] = True
