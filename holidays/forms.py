5b2fc2596ef66c923229c4e0ac337261a4e4d5ef
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-03-04 20:25:06 +0000

    Amended package form

--- holidays/forms.py
+++ holidays/forms.py
@@ -21,24 +21,8 @@ class PackageForm(forms.ModelForm):
         self.helper.field_class = 'mb-3'
         self.helper.label_class = 'form-label'
 
-        categories = Category.objects.all()
-        category_names = [(c.id, c.name) for c in categories]
-        countries = Country.objects.all()
-        country_names = [(c.id, c.name) for c in countries]
-        features = Feature.objects.all()
-        features_names = [(f.id, f.name) for f in features]
-        activities = Activity.objects.all()
-        activities_names = [(a.id, a.activity) for a in activities]
-        extras = Extra.objects.all()
-        extra_names = [(e.id, e.name) for e in extras]
-
         self.fields['name'].widget.attrs['autofocus'] = True
         self.fields['image'].widget = CustomClearableFileInput()
-        self.fields['category'].choices = category_names
-        self.fields['country'].choices = country_names
-        self.fields['features'].choices = features_names
-        self.fields['activities'].choices = activities_names
-        self.fields['extras'].choices = extra_names
         self.fields['description'].widget.attrs['rows'] = 5
 
 
