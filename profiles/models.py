3d77c5d5eefcae59155e68d98836cc6b5f9a4c21
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-03-17 17:50:04 +0000

    Added user profile signal and updated user signal

--- profiles/models.py
+++ profiles/models.py
@@ -1,11 +1,6 @@
 from django.db import models
-from django.conf import settings
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
 from django_countries.fields import CountryField
-import stripe
 
 
 class UserProfile(models.Model):
@@ -26,13 +21,3 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.get_full_name()
-
-
-@receiver(post_save, sender=User)
-def create_or_update_user_profile(sender, instance, created, **kwargs):
-    """
-    Create or update the user profile
-    """
-    if created:
-        UserProfile.objects.create(user=instance)
-    instance.userprofile.save()
---
+++ profiles/signals.py
@@ -0,0 +1,42 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.conf import settings
+from .models import UserProfile
+from django.contrib.auth.models import User
+import stripe
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    Create or update the user profile and create stripe customer
+    """
+    if created:
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        UserProfile.objects.create(user=instance)
+        customer = stripe.Customer.create(
+            name=instance.get_full_name(),
+            email=instance.email
+        )
+        instance.userprofile.stripe_customer_id = customer['id']
+    instance.userprofile.save()
+
+
+@receiver(post_save, sender=UserProfile)
+def stripe_update_on_save(sender, instance, created, **kwargs):
+    """
+    Update stripe customer details on update
+    """
+    if not created:
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.Customer.modify(
+            instance.stripe_customer_id,
+            email=instance.user.email,
+            address={
+                'line1': instance.street_address1,
+                'line2': instance.street_address2,
+                'city': instance.town_or_city,
+                'state': instance.county,
+                'country': instance.country,
+                'postal_code': instance.postcode
+            }
+        )
