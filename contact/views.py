935a45578f7e1c3313368742a9dd2294b1ce7928
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-05-05 17:48:57 +0100

    Added logic for form submission to contact view

--- contact/views.py
+++ contact/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from .forms import ContactForm
 
 def contact(request):
     """A view to retun the contact page"""
+
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+
+        if form.is_valid:
+            form.save()
+            return HttpResponse(200)
 
     form = ContactForm()
     template = 'contact/contact.html'
