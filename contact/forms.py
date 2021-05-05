a01537c74e6e3d9a2799f45f9e4dacc8b36a19e0
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-05-05 17:00:50 +0100

    Added contact form

---
+++ contact/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+from holidays.widgets import CustomClearableFileInput
+from .models import Contact
+from crispy_forms.helper import FormHelper
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        exclude = ('date',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.field_class = 'mb-4'
+        self.helper.label_class = 'form-label'
+
+        placeholders = {
+            'name': 'Full Name',
+            'email': 'Email Address',
+            'subject': 'Subject',
+            'message': 'Message',
+        }
+
+        subjects = [
+            ('', 'Subject *'),
+            ('Holiday Information', 'Holiday Information'),
+            ('Offers', 'Offers'),
+            ('Bookings', 'Bookings'),
+            ('General Enquiries', 'General Enquiries'),
+            ('Other', 'Other')
+        ]
+
+        self.fields['subject'] = forms.ChoiceField(choices=subjects)
+        self.fields['name'].widget.attrs['autofocus'] = True
+        self.fields['message'].widget.attrs['rows'] = 6
+
+        for field in self.fields:
+            if field != 'subject':
+                placeholder = f'{placeholders[field]} *'
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].label = False
--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .forms import ContactForm
 
 def contact(request):
     """A view to retun the contact page"""
+
+    form = ContactForm()
+    template = 'contact/contact.html'
+    context = {
+        'form': form,
+    }
     
-    return render(request, 'contact/contact.html')
+    return render(request, template, context)
