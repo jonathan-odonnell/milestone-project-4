6592890d59a38045a0e7b3490033a31ba2623fa3
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-05-05 18:15:49 +0100

    Amended contact admin view

--- contact/admin.py
+++ contact/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from .models import Contact
 
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('subject', 'name',)
+
+    list_display = ('subject', 'name', 'date',)
+    readonly_fields = ('date',)
+    fields = ('name', 'email', 'date', 'subject', 'message',)
     list_per_page = 20
 
 
