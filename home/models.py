502942398c571200da74a127b2cb82727ce33c2b
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-05-09 19:31:33 +0100

    Added newsletter sign up model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class NewsletterSignUp(models.Model):
+
+    email = models.EmailField(max_length=254)
+
+    def __str__(self):
+        return self.email
