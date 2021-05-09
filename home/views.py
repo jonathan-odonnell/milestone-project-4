9165473cf4b5a25ca6b9f37273f240e74a905903
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-05-09 20:03:48 +0100

    Added newsletter view and url

--- home/views.py
+++ home/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.views.decorators.http import require_POST
 from django.db.models import Count
+from .models import NewsletterSignUp
 from holidays.models import Package
 from django.conf import settings
 
 # Create your views here.
+
 
 def index(request):
     """A view to retun the index page"""
@@ -19,13 +22,23 @@ def index(request):
         name = f'settings.OFFER_DESTINATION_{i}'
         offers.append(eval(name))
 
-    holidays = Package.objects.filter(name__in=holidays).annotate(num_reviews=Count('reviews'))
+    holidays = Package.objects.filter(
+        name__in=holidays).annotate(num_reviews=Count('reviews'))
     offers = Package.objects.filter(name__in=offers)
-    
+
     template = 'home/index.html'
     context = {
         'holidays': holidays,
         'offers': offers,
     }
-    
+
     return render(request, template, context)
+
+
+@require_POST
+def newsletter(request):
+    """A view to add the customer's email to the newsletter sign ups database"""
+    sign_up = NewsletterSignUp(email=request.POST['newsletter'])
+    sign_up.save()
+
+    return HttpResponse(200)
