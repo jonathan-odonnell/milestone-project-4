baf1e880572071816d937b019d57b9f2e3a37d01
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-05-14 23:30:19 +0100

    Refactored code and added error messages in checkout and cache checkout views

--- checkout/views.py
+++ checkout/views.py
@@ -1,5 +1,6 @@
 from django.views.decorators.http import require_POST
 from django.shortcuts import render, HttpResponse, redirect, reverse, get_object_or_404
+from django.contrib import messages
 from django.http import JsonResponse
 from .forms import BookingForm
 from booking.models import Booking, BookingPackage
@@ -27,71 +28,39 @@ def cache_checkout_data(request):
         })
         return HttpResponse(status=200)
     except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
         return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
-    stripe.api_key = stripe_secret_key
     booking_number = request.session.get('booking_number', '')
     profile = None
+    cards = None
 
     if not booking_number:
         return redirect(reverse('booking'))
 
     if request.method == 'POST':
-        profile = None
         form_data = None
         save_info = 'save_info' in request.POST
-        saved_card = 'card' in request.POST
-        payment_type = request.POST['payment']
         pid = request.POST.get('client_secret').split('_secret')[0]
         booking = Booking.objects.get(booking_number=booking_number)
         stripe.api_key = settings.STRIPE_SECRET_KEY
 
-        if request.user.is_authenticated and saved_card:
-            profile = UserProfile.objects.get(user=request.user)
-            form_data = {
-                'full_name': profile.user.get_full_name(),
-                'email': profile.user.email,
-                'phone_number': profile.phone_number,
-                'street_address1': profile.street_address1,
-                'street_address2': profile.street_address2,
-                'town_or_city': profile.town_or_city,
-                'county': profile.county,
-                'country': profile.country,
-                'postcode': profile.postcode,
-            }
-
-        elif payment_type == 'payment_button':
-            intent = stripe.PaymentMethod.retrieve(pid)
-            billing_details = intent.charges.data[0].billing_details
-            shipping_details = intent.shipping
-            form_data = {
-                'full_name': shipping_details.name,
-                'email': billing_details.email,
-                'phone_number': shipping_details.phone,
-                'street_address1': shipping_details.address.line1,
-                'street_address2': shipping_details.address.line2,
-                'town_or_city': shipping_details.address.city,
-                'county': shipping_details.address.state,
-                'country': shipping_details.address.country,
-                'postcode': shipping_details.address.postal_code
-            }
-
-        else:
-            form_data = {
-                'full_name': request.POST['full_name'],
-                'email': request.POST['email'],
-                'phone_number': request.POST['phone_number'],
-                'street_address1': request.POST['street_address1'],
-                'street_address2': request.POST['street_address2'],
-                'town_or_city': request.POST['town_or_city'],
-                'county': request.POST['county'],
-                'country': request.POST['country'],
-                'postcode': request.POST['postcode'],
-            }
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'town_or_city': request.POST['town_or_city'],
+            'county': request.POST['county'],
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+        }
 
         booking_form = BookingForm(form_data, instance=booking)
 
@@ -102,6 +71,7 @@ def checkout(request):
             booking.save()
 
             if request.user.is_authenticated:
+                profile = UserProfile.objects.get(user=request.user)
                 booking.user_profile = profile
                 booking.save()
 
@@ -123,11 +93,15 @@ def checkout(request):
 
             return redirect(reverse('checkout_success', args=[booking.booking_number]))
 
+        else:
+            messages.error(request, 'There was an error with your form. \
+                Please double check your information.')
+
     else:
         booking = booking_details(request)
         total = booking['total']
         stripe_total = round(total * 100)
-        cards = None
+        stripe.api_key = stripe_secret_key
 
         if request.user.is_authenticated:
             profile = UserProfile.objects.get(user=request.user)
@@ -143,12 +117,6 @@ def checkout(request):
                 'postcode': profile.postcode,
             })
 
-            cards = stripe.PaymentMethod.list(
-                customer=profile.stripe_customer_id,
-                type="card",
-                limit=3
-            )
-
             intent = stripe.PaymentIntent.create(
                 amount=stripe_total,
                 currency=settings.STRIPE_CURRENCY,
@@ -156,11 +124,18 @@ def checkout(request):
             )
 
         else:
+            booking_form = BookingForm()
             intent = stripe.PaymentIntent.create(
                 amount=stripe_total,
                 currency=settings.STRIPE_CURRENCY
             )
-            booking_form = BookingForm()
+
+    if request.user.is_authenticated:
+        cards = stripe.PaymentMethod.list(
+                customer=profile.stripe_customer_id,
+                type="card",
+                limit=3
+            )
 
     context = {
         'booking_form': booking_form,
