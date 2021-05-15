489793341356522bc2c25662174fc383b86c1a0b
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-05-15 22:21:37 +0100

    Added confirmation email templates and logic in webhook handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
 from booking.models import Booking, BookingPackage
 from holidays.models import Package
 from profiles.models import UserProfile
@@ -13,6 +16,23 @@ class StripeWH_Handler:
 
     def __init__(self, request):
         self.request = request
+
+    def _send_confirmation_email(self, booking):
+        """Send the user a confirmation email"""
+        cust_email = booking.email
+        subject = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_subject.txt',
+            {'booking': booking})
+        body = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_body.txt',
+            {'booking': booking, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+        
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            [cust_email]
+        )        
 
     def handle_event(self, event):
         """
@@ -59,6 +79,7 @@ class StripeWH_Handler:
                 time.sleep(1)
 
         if booking_exists:
+            self._send_confirmation_email(booking)
             return HttpResponse(
                 content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
                 status=200)
@@ -109,6 +130,7 @@ class StripeWH_Handler:
                     content=f'Webhook received: {event["type"]} | ERROR: {e}',
                     status=500)
 
+        self._send_confirmation_email(booking)
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
