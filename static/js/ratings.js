f05d77af1b739c1cb0f04c23ddaf9295883fe421
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-05-17 00:35:18 +0100

    Amended rating stars javascript

--- static/js/ratings.js
+++ static/js/ratings.js
@@ -1,14 +1,14 @@
 function generateStars() {
-    let fullStar = "<i class='fas fa-star'></i>"
-    let halfStar = "<i class='fas fa-star-half-alt'></i>"
-    let emptyStar = "<i class='far fa-star'></i>"
+    let fullStar = '<i class="fas fa-star"></i>'
+    let halfStar = '<i class="fas fa-star-half-alt"></i>'
+    let emptyStar = '<i class="far fa-star"></i>'
     $('.rating').each(function () {
         rating = parseFloat($(this).html())
         displayStars = ""
         for (let i = 1; i <= 5; i++) {
-            if (Math.round(rating) >= i) {
+            if (rating >= i) {
                 displayStars += fullStar
-            } else if (rating >= i - 0.5) {
+            } else if (Math.ceil(rating) === i && !Number.isInteger(rating)) {
                 displayStars += halfStar
             } else {
                 displayStars += emptyStar
