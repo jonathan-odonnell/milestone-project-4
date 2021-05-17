b6ddc1526a228de38da46033679c6db97a742e7b
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-05-17 19:23:20 +0100

    Amended flight models and admin view

--- flights/admin.py
+++ flights/admin.py
@@ -3,9 +3,9 @@ from .models import Flight
 
 
 class FlightAdmin(admin.ModelAdmin):
-    list_display = ('name', 'origin', 'destination',)
+    list_display = ('flight_number', 'origin', 'destination',)
 
-    ordering = ('name',)
+    ordering = ('flight_number',)
 
     list_per_page = 20
 
--- flights/models.py
+++ flights/models.py
@@ -2,14 +2,24 @@ from django.db import models
 
 class Flight(models.Model):
 
-    name = models.CharField(max_length=6)
-    origin = models.CharField(max_length=254)
-    destination = models.CharField(max_length=254)
-    departure_time = models.DateTimeField()
-    arrival_time = models.DateTimeField()
-    duration = models.DurationField()
-    layover = models.CharField(max_length=254, null=True, blank=True)
-    baggage = models.DecimalField(max_digits=2, decimal_places=0)
+    class Meta:
+        ordering = ('flight_number',)
+
+    CHOICES = [
+            ('', 'Direction'),
+            ('Outbound', 'Outbound'),
+            ('Return', 'Return'),
+        ]
+
+    flight_number = models.CharField(max_length=5, null=False, blank=False)
+    direction = models.CharField(max_length=50, null=False, blank=False, choices=CHOICES)
+    origin = models.CharField(max_length=50, null=False, blank=False)
+    destination = models.CharField(max_length=50, null=False, blank=False)
+    departure_time = models.DateTimeField(null=False, blank=False)
+    arrival_time = models.DateTimeField(null=False, blank=False)
+    duration = models.DurationField(null=False, blank=False)
+    layover = models.CharField(max_length=50, null=True, blank=True)
+    baggage = models.DecimalField(max_digits=2, decimal_places=0, null=False, blank=False)
 
     def __str__(self):
         return self.name
