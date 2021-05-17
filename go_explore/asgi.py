a334119c1f3a976075da3bc5f279c100f493f253
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-05-17 23:24:26 +0000

    Updated settings file location in asgi.py

--- go_explore/asgi.py
+++ go_explore/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for travel project.
+ASGI config for go_explore project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'travel.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'go_explore.settings')
 
 application = get_asgi_application()
