01935344cd5ab0f6085f9dc6e63597f502aacd37
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-05-18 17:02:40 +0100

    Updated allowed hosts in settings

--- go_explore/settings.py
+++ go_explore/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEVELOPMENT')
 
-ALLOWED_HOSTS = ['go-explore.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['go-explore.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
