e3130e6b45ce9d464114a2c386d33154b9953f6d
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-05-24 14:46:50 +0100

    Amended flights admin view

--- flights/admin.py
+++ flights/admin.py
@@ -3,10 +3,17 @@ from .models import Flight
 
 
 class FlightAdmin(admin.ModelAdmin):
+    readonly_fields = ('duration',)
+
+    fields = (
+        'flight_number', 'direction', 'origin', 
+        'destination', 'departure_time', 'arrival_time', 
+        'duration', 'destination_time_zone', 'layover', 'baggage')
+    
     list_display = ('flight_number', 'origin', 'destination',)
-
+    
     ordering = ('flight_number',)
-
+    
     list_per_page = 20
 
 admin.site.register(Flight, FlightAdmin)
