5af6cc650466db475ae5746d1e1533d2d2fd5e14
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-05-24 15:20:38 +0100

    Amended booking models

--- booking/models.py
+++ booking/models.py
@@ -3,15 +3,17 @@ import uuid
 from django.db import models
 from django.db.models import Sum
 from django.conf import settings
+from django.db.models.deletion import SET_NULL
+from django.db.models.expressions import F
 from django_countries.fields import CountryField
 from profiles.models import UserProfile
 from holidays.models import Package
 from flights.models import Flight
 from extras.models import Extra
-import datetime
+
 
 class Coupon(models.Model):
-    
+
     name = models.CharField(max_length=254)
     start_date = models.DateField()
     end_date = models.DateField()
@@ -22,45 +24,48 @@ class Coupon(models.Model):
 
 
 class Booking(models.Model):
-    booking_number = models.CharField(
-        max_length=32, null=False, editable=False)
-    full_name = models.CharField(max_length=50, null=False, blank=False)
+    booking_number = models.CharField(max_length=32)
+    full_name = models.CharField(max_length=50)
     user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
                                      null=True, blank=True, related_name='bookings')
-    email = models.EmailField(max_length=254, null=False, blank=False, default='')
-    phone_number = models.CharField(max_length=20, null=False, blank=False, default='')
-    town_or_city = models.CharField(max_length=40, null=False, blank=False, default='')
-    street_address1 = models.CharField(max_length=80, null=False, blank=False, default='')
-    street_address2 = models.CharField(max_length=80, null=True, blank=True, default='')
-    county = models.CharField(max_length=80, null=False, blank=False, default='')
-    country = CountryField(blank_label='Country *', null=False, blank=False, default='')
-    postcode = models.CharField(max_length=20, null=False, blank=False, default='')
+    email = models.EmailField(
+        max_length=254, null=False, blank=False, default='')
+    phone_number = models.CharField(max_length=20, default='')
+    town_or_city = models.CharField(max_length=40, default='')
+    street_address1 = models.CharField(max_length=80, default='')
+    street_address2 = models.CharField(
+        max_length=80, null=True, blank=True, default='')
+    county = models.CharField(max_length=80, default='')
+    country = CountryField(blank_label='Country *', default='')
+    postcode = models.CharField(max_length=20, default='')
     date = models.DateTimeField(auto_now_add=True)
-    coupon = models.CharField(max_length=20, null=True, blank=True)
+    guests = models.IntegerField(default=0)
+    departure_date = models.DateField()
+    return_date = models.DateField()
+    package = models.OneToOneField(Package, null=True, blank=True, on_delete=SET_NULL)
+    outbound_flight = models.ForeignKey(Flight, null=True, blank=True, on_delete=models.SET_NULL, related_name='outbound_flight')
+    return_flight = models.ForeignKey(Flight, null=True, blank=True, on_delete=models.SET_NULL, related_name='return_flight')
     subtotal = models.DecimalField(
-        max_digits=10, decimal_places=2, null=False, default=0)
+        max_digits=10, decimal_places=2, editable=False, default=0)
     extras_total = models.DecimalField(
-        max_digits=10, decimal_places=2, null=False, default=0)
+        max_digits=10, decimal_places=2, editable=False, default=0)
+    coupon = models.CharField(max_length=20, null=True,
+                              blank=True, editable=False)
     discount = models.DecimalField(
-        max_digits=10, decimal_places=2, null=False, default=0, editable=False)
+        max_digits=10, decimal_places=2, editable=False, default=0)
     grand_total = models.DecimalField(
-        max_digits=10, decimal_places=2, null=False, default=0)
-    paid = models.BooleanField(null=False, blank=False, default=False, editable=False)
-    stripe_pid = models.CharField(
-        max_length=254, null=False, blank=False, default='')
-    paypal_pid = models.CharField(
-        max_length=254, null=False, blank=False, default='')
+        max_digits=10, decimal_places=2, editable=False, default=0)
+    paid = models.BooleanField(editable=False, default=False)
+    stripe_pid = models.CharField(max_length=254, default='')
+    paypal_pid = models.CharField(max_length=254, default='')
 
     def _generate_booking_number(self):
         return uuid.uuid4().hex.upper()
 
     def update_totals(self):
-        package_total = self.booking_package.total
-        extras_total = self.booking_extras.aggregate(Sum('total'))[
+        self.extras_total = self.booking_extras.aggregate(Sum('total'))[
             'total__sum'] or 0
-        self.subtotal = package_total
-        self.extras_total = extras_total
-        self.grand_total = package_total + extras_total - self.discount
+        self.grand_total = self.subtotal + self.extras_total - self.discount
         self.save()
 
     def save(self, *args, **kwargs):
@@ -69,43 +74,21 @@ class Booking(models.Model):
         if self.coupon:
             coupon_qs = Coupon.objects.get(name=self.coupon)
             self.discount = coupon_qs.amount
+        self.subtotal = self.package.price * self.guests
         super().save(*args, **kwargs)
 
     def __str__(self):
         return self.booking_number
 
 
-class BookingPackage(models.Model):
-    booking = models.OneToOneField(
-        Booking, null=False, blank=False, on_delete=models.CASCADE, related_name='booking_package')
-    package = models.ForeignKey(
-        Package, null=False, blank=False, on_delete=models.CASCADE)
-    guests = models.IntegerField(null=False, blank=False)
-    departure_date = models.DateField(null=False, blank=False)
-    return_date = models.DateField(null=False, blank=False)
-    outbound_flight = models.ForeignKey(
-        Flight, null=True, blank=True, on_delete=models.SET_NULL, related_name='booking_outbound_flight')
-    return_flight = models.ForeignKey(
-        Flight, null=True, blank=True, on_delete=models.SET_NULL, related_name='booking_inbound_flight')
-    total = models.DecimalField(
-        max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
-
-    def save(self, *args, **kwargs):
-        self.total = self.package.price * self.guests
-        super().save(*args, **kwargs)
-
-    def __str__(self):
-        return f'{self.package.name} on booking {self.booking.booking_number}'
-
-
 class BookingExtra(models.Model):
     booking = models.ForeignKey(
-        Booking, null=False, blank=False, on_delete=models.CASCADE, related_name='booking_extras')
+        Booking, on_delete=models.CASCADE, related_name='booking_extras')
     extra = models.ForeignKey(
-        Extra, null=False, blank=False, on_delete=models.CASCADE)
-    quantity = models.IntegerField(null=False, blank=False, default=0)
+        Extra, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
     total = models.DecimalField(
-        max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+        max_digits=6, decimal_places=2, editable=False)
 
     def save(self, *args, **kwargs):
         self.total = self.extra.price * self.quantity
@@ -117,10 +100,10 @@ class BookingExtra(models.Model):
 
 class BookingPassenger(models.Model):
     booking = models.ForeignKey(
-        Booking, null=False, blank=False, on_delete=models.CASCADE, related_name='booking_passengers')
-    full_name = models.CharField(max_length=50, null=False, blank=False)
-    date_of_birth = models.DateField(null=False, blank=False)
-    passport_number = models.DecimalField(max_digits=9, decimal_places=0, null=False, blank=False)
+        Booking, on_delete=models.CASCADE, related_name='booking_passengers')
+    full_name = models.CharField(max_length=50)
+    date_of_birth = models.DateField()
+    passport_number = models.DecimalField(max_digits=9, decimal_places=0)
 
     def __str__(self):
         return f'{self.full_name} on booking {self.booking.booking_number}'
