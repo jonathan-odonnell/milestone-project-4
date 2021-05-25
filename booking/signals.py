b704cde135286831d9447d9f542a73eb13335927
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-05-25 22:49:35 +0100

    Fixed an issue with the booking signals causing an infinite loop

--- booking/signals.py
+++ booking/signals.py
@@ -1,20 +1,6 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-from .models import Booking, BookingExtra
-
-@receiver(post_save, sender=Booking)
-def booking_update_on_save(sender, instance, created, **kwargs):
-    """
-    Update order total on booking extras update/create
-    """
-    instance.update_totals()
-
-@receiver(post_delete, sender=Booking)
-def booking_update_on_delete(sender, instance, **kwargs):
-    """
-    Update order total on booking extras delete
-    """
-    instance.update_totals()
+from .models import BookingExtra
 
 @receiver(post_save, sender=BookingExtra)
 def extra_update_on_save(sender, instance, created, **kwargs):
