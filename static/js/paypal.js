f7715fd9271ca9334e3419b19db4bdc208c85fdb
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-05-26 23:03:14 +0100

    Amended the paypal javascript to submit the form once the payment has been approved

--- static/js/paypal.js
+++ static/js/paypal.js
@@ -23,20 +23,7 @@ paypal.Buttons({
             headers: {
                 'content-type': 'application/json',
                 'X-CSRFToken': csrfToken
-            },
-            body: JSON.stringify({
-                'full_name': $('#payment-form').find('#id_full_name').val(),
-                'email': $('#payment-form').find('#id_email').val(),
-                'phone_number': $('#payment-form').find('#id_phone_number').val(),
-                'street_address1': $('#payment-form').find('#id_street_address1').val(),
-                'street_address2': $('#payment-form').find('#id_street_address2').val(),
-                'town_or_city': $('#payment-form').find('#id_town_or_city').val(),
-                'county': $('#payment-form').find('#id_county').val(),
-                'country': $('#payment-form').find('#id_country').val(),
-                'postcode': $('#payment-form').find('#id_postcode').val(),
-                'save_info': $('#payment-form').find('#id_save_info').val(),
-            })
-        }).then(function (res) {
+        }}).then(function (res) {
             return res.json();
         }).then(function (data) {
             return data.id;
@@ -55,7 +42,8 @@ paypal.Buttons({
         }).then(function (res) {
             return res.json();
         }).then(function (details) {
-            location.replace(`/checkout/checkout_success/${details.purchase_units[0].reference_id}/`)
+            $('input[name="paypal_pid"]').val(details.id)
+            $('#payment-form').submit()
         })
     }
 }).render('#paypal');
