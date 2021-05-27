d32e512e58a412b5658e9789252391abe84e8e56
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-05-27 21:29:02 +0100

    Amended datepicker min and max dates and added comments in flights.js

--- static/js/flights.js
+++ static/js/flights.js
@@ -1,27 +1,43 @@
-let today = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate());
+/* Sets start date and end date. Code is from https://gijgo.com/datepicker/configuration/minDate and 
+https://gijgo.com/datepicker/configuration/maxDate */
+let startDate = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate() + 14);
+let endDate = new Date(new Date().getFullYear() + 1, new Date().getMonth(), new Date().getDate() + 14);
+
+/* Adds the blank option to the destination time zone input and removes the default datepicker input
+ styling from the departure time and arrival time inputs */
 $(document).ready(function () {
-    $('#div_id_departure_time input').removeClass('gj-textbox-md')
-    $('#div_id_arrival_time input').removeClass('gj-textbox-md')
-    $('#id_destination_time_zone').prepend('<option value="" selected></option>')
+	$('#div_id_departure_time input').removeClass('gj-textbox-md')
+	$('#div_id_arrival_time input').removeClass('gj-textbox-md')
+	$('#id_destination_time_zone').prepend('<option value="" selected></option>')
 })
+
+/* Configures the departure time datetime picker. Code is from https://gijgo.com/datetimepicker/, 
+https://gijgo.com/datepicker/configuration and https://gijgo.com/datetimepicker/configuration */
 $('#id_departure_time').datetimepicker({
-    footer: true,
-    modal: true,
-    datepicker: {
-        minDate: today,
-        showRightIcon: false,
-    },
-    format: 'dd/mm/yyyy HH:MM'
+	footer: true,
+	modal: true,
+	datepicker: {
+		minDate: startDate,
+		maxDate: endDate,
+		showRightIcon: false,
+	},
+	format: 'dd/mm/yyyy HH:MM'
 });
+
+/* Configures the arrival time datetime picker. Code is from https://gijgo.com/datetimepicker/, 
+https://gijgo.com/datepicker/configuration and https://gijgo.com/datetimepicker/configuration */
 $('#id_arrival_time').datetimepicker({
-    footer: true,
-    modal: true,
-    datepicker: {
-        minDate: today,
-        showRightIcon: false,
-    },
-    format: 'dd/mm/yyyy HH:MM'
+	footer: true,
+	modal: true,
+	datepicker: {
+		minDate: startDate,
+		maxDate: endDate,
+		showRightIcon: false,
+	},
+	format: 'dd/mm/yyyy HH:MM'
 });
-$("#id_dearture_time, #id_arrival_time").keydown(function (e) {
-    e.preventDefault();
+
+// Prevents the user from being able to enter text in the departure time or arrival time fields.
+$('#id_dearture_time, #id_arrival_time').keydown(function (e) {
+	e.preventDefault();
 });
