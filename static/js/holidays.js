d6e187ea9af718d54577476c5bd73aeb063f26e0
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-05-28 16:41:48 +0100

    Added comments to holidays.js

--- static/js/holidays.js
+++ static/js/holidays.js
@@ -1,15 +1,24 @@
-// jQuery for each function is from https://stackoverflow.com/questions/17258119/jquery-each-run-function-for-each-instance-of-selector
-let currentUrl = new URL(window.location)
+// Stores the page's URL.
+let currentUrl = new URL(window.location);
 
+/* Updates the page search perameter in the currentUrl variable when a page link is clicked. 
+An AJAX get request for the URL is then submitted and the HTML inside the holidays ID is replaced
+with the html returned and the generateStars function is called to generate new star ratings. Code for
+the delegate target jQuery is from https://api.jquery.com/event.delegateTarget/#event-delegateTarget1 */
 $('#holidays').on('click', '.page-link', function () {
     let page = $(this).data('page');
     currentUrl.searchParams.set("page", page);
     $.get(currentUrl).done(function (data) {
-        $('#holidays').html(data.holidays)
-        generateStars()
-    })
-})
+        $('#holidays').html(data.holidays);
+        generateStars();
+    });
+});
 
+
+/* Updates the sort, direction and page search perameters in the currentUrl variable when the value of 
+the sort-selector ID changes. An AJAX get request for the URL is then submitted and the HTML inside the
+holidays ID is replaced with the html returned and the generateStars function is called to generate new
+star ratings. */
 $('#sort-selector').change(function () {
     let sortSelector = $(this).val();
     currentUrl.searchParams.set("page", 1);
@@ -23,45 +32,55 @@ $('#sort-selector').change(function () {
         currentUrl.searchParams.delete("direction");
     }
     $.get(currentUrl).done(function (data) {
-        $('#holidays').html(data.holidays)
-        generateStars()
-    })
-})
+        $('#holidays').html(data.holidays);
+        generateStars();
+    });
+});
+
+/* Updates the categories and page search perameters in the currentUrl variable when one of the category
+filters is toggled on or off. An AJAX get request for the URL is then submitted and the HTML inside the
+holidays ID is replaced with the html returned and the generateStars function is called to generate new
+star ratings. Code for the toggleClass jQuery is from https://api.jquery.com/toggleClass/#toggleClass1 */
 
 $('#category-filters a').on('click', function () {
     $(this).find('span').toggleClass('bg-primary text-dark');
-    let categories = []
+    let categories = [];
     currentUrl.searchParams.set("page", 1);
     $('#category-filters').find('.bg-primary').each(function () {
-        categories.push($(this).text().toLowerCase().replace(' ', '_'))
-    })
+        categories.push($(this).text().toLowerCase().replace(' ', '_'));
+    });
     if (categories.length > 0) {
-        categories = categories.join(',')
-        currentUrl.searchParams.set("categories", categories)
+        categories = categories.join(',');
+        currentUrl.searchParams.set("categories", categories);
     } else {
         currentUrl.searchParams.delete("categories");
     }
     $.get(currentUrl).done(function (data) {
-        $('#holidays').html(data.holidays)
-        generateStars()
-    })
-})
+        $('#holidays').html(data.holidays);
+        generateStars();
+    });
+});
+
+/* Updates the countries and page search perameters in the currentUrl variable when one of the coountry
+filters is toggled on or off. An AJAX get request for the URL is then submitted and the HTML inside the
+holidays ID is replaced with the html returned and the generateStars function is called to generate new
+star ratings. Code for the toggleClass jQuery is from https://api.jquery.com/toggleClass/#toggleClass1 */
 
 $('#country-filters a').on('click', function () {
     $(this).find('span').toggleClass('bg-primary text-dark');
-    let countries = []
+    let countries = [];
     currentUrl.searchParams.set("page", 1);
     $('#country-filters').find('.bg-primary').each(function () {
-        countries.push($(this).text().toLowerCase().replace(' ', '_'))
-    })
+        countries.push($(this).text().toLowerCase().replace(' ', '_'));
+    });
     if (countries.length > 0) {
-        countries = countries.join(',')
-        currentUrl.searchParams.set("countries", countries)
+        countries = countries.join(',');
+        currentUrl.searchParams.set("countries", countries);
     } else {
         currentUrl.searchParams.delete("countries");
     }
     $.get(currentUrl).done(function (data) {
-        $('#holidays').html(data.holidays)
-        generateStars()
-    })
-})
+        $('#holidays').html(data.holidays);
+        generateStars();
+    });
+});
