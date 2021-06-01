0bcc3ebc431bf8ef6737f908c62bd8f90e80d410
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-06-01 20:51:02 +0100

    Added origin time zone field to flights model and added logic to convert non local times times entered to UTC times on save

--- flights/models.py
+++ flights/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models.fields import BLANK_CHOICE_DASH
 from timezone_field import TimeZoneField
+import pytz
 
 class Flight(models.Model):
 
@@ -18,13 +19,24 @@ class Flight(models.Model):
     origin = models.CharField(max_length=50)
     destination = models.CharField(max_length=50)
     departure_time = models.DateTimeField()
+    origin_time_zone = TimeZoneField()
     arrival_time = models.DateTimeField()
-    duration = models.DurationField()
     destination_time_zone = TimeZoneField()
+    duration = models.DurationField()
     layover = models.CharField(max_length=50, null=True, blank=True)
     baggage = models.DecimalField(max_digits=2, decimal_places=0)
 
     def save(self, *args, **kwargs):
+        if self.direction == 'Outbound':
+            tz = self.destination_time_zone
+            self.arrival_time = self.arrival_time.replace(tzinfo=None)
+            self.arrival_time = tz.localize(self.arrival_time)
+            self.arrival_time = self.arrival_time.astimezone(pytz.utc)
+        else:
+            tz = self.origin_time_zone
+            self.departure_time = self.departure_time.replace(tzinfo=None)
+            self.departure_time = tz.localize(self.departure_time)
+            self.departure_time = self.departure_time.astimezone(pytz.utc)
         self.duration = self.arrival_time - self.departure_time
         super().save(*args, **kwargs)
 
