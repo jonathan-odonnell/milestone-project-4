3116ad35afda6c8ecf5280d7b22d1c105f3995d4
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-06-03 16:01:28 +0100

    Removed redundant code and amended comments in holidays.js

--- static/js/holidays.js
+++ static/js/holidays.js
@@ -2,23 +2,16 @@
 for the filterUrl is then submitted and the HTML inside the holidays ID is replaced with the HTML returned in
 the response and the generateStars function is called to generate new star ratings. A new entry is added to the
 browser's session history to update the page URL. Code for the delegate target jQuery is from 
-https://api.jquery.com/event.delegateTarget/#event-delegateTarget1. */
+https://api.jquery.com/event.delegateTarget/#event-delegateTarget1 and code for adding a new entry to the browser's
+session history is from https://developer.mozilla.org/en-US/docs/Web/API/History/pushState. */
 
 $('#holidays').on('click', '.page-link', function () {
     let page = $(this).data('page');
     let currentUrl = new URL(window.location);
-    /* Code for replacing the URL pathname is from 
-    https://stackoverflow.com/questions/61896535/how-to-change-js-url-object-pathname-and-protocol-properties */
-    currentUrl.pathname = `${currentUrl.pathname}filter/`;
     currentUrl.searchParams.set('page', page);
     $.get(currentUrl).done(function (data) {
         $('#holidays').html(data.holidays);
         generateStars();
-        /* Code for replacing the URL pathname is from 
-        https://stackoverflow.com/questions/61896535/how-to-change-js-url-object-pathname-and-protocol-properties */
-        currentUrl.pathname = currentUrl.pathname.replace('filter/', '');
-        /*  Code for adding a new entry to the browser's session history is from
-        https://developer.mozilla.org/en-US/docs/Web/API/History/pushState */
         window.history.pushState({}, '', currentUrl);
     });
 });
@@ -27,14 +20,12 @@ $('#holidays').on('click', '.page-link', function () {
 /* Updates the currentUrl pathname and sort, direction and page search perameters when the value of the sort-selector ID
 changes. An AJAX get request for the currentUrl is then submitted and the HTML inside the holidays ID is replaced with the
 HTML returned in the response and the generateStars function is called to generate new star ratings. A new entry is added
-to the browser's session history to update the page URL. */
+to the browser's session history to update the page URL and code for adding a new entry to the browser's session history 
+is from https://developer.mozilla.org/en-US/docs/Web/API/History/pushState. */
 
 $('#sort-selector').change(function () {
     let sortSelector = $(this).val();
     let currentUrl = new URL(window.location);
-    /* Code for replacing the URL pathname is from 
-    https://stackoverflow.com/questions/61896535/how-to-change-js-url-object-pathname-and-protocol-properties */
-    currentUrl.pathname = `${currentUrl.pathname}filter/`;
     currentUrl.searchParams.delete('page');
     if (sortSelector !== 'reset') {
         let sort = sortSelector.split("_")[0];
@@ -48,11 +39,6 @@ $('#sort-selector').change(function () {
     $.get(currentUrl).done(function (data) {
         $('#holidays').html(data.holidays);
         generateStars();
-        /* Code for replacing the URL pathname is from 
-        https://stackoverflow.com/questions/61896535/how-to-change-js-url-object-pathname-and-protocol-properties */
-        currentUrl.pathname = currentUrl.pathname.replace('filter/', '');
-        /*  Code for adding a new entry to the browser's session history is from
-        https://developer.mozilla.org/en-US/docs/Web/API/History/pushState */
         window.history.pushState({}, '', currentUrl);
     });
 });
@@ -61,17 +47,15 @@ $('#sort-selector').change(function () {
 filters is toggled on or off. An AJAX get request for the filterUrl is then submitted and the HTML inside
 the holidays ID is replaced with the HTML returned in the response and the generateStars function is called
 to generate new star ratings. A new entry is added to the browser's session history to update the page URL.
-Code for the toggleClass jQuery method is from https://api.jquery.com/toggleClass/#toggleClass1 */
+Code for the toggleClass jQuery method is from https://api.jquery.com/toggleClass/#toggleClass1, code for the
+each jQuery method is from https://api.jquery.com/each/#each-function and code for adding a new entry to the 
+browser's session history is from https://developer.mozilla.org/en-US/docs/Web/API/History/pushState */
 
 $('#category-filters a').on('click', function () {
     $(this).find('span').toggleClass('bg-primary text-dark');
     let categories = [];
     let currentUrl = new URL(window.location);
-    /* Code for replacing the URL pathname is from 
-    https://stackoverflow.com/questions/61896535/how-to-change-js-url-object-pathname-and-protocol-properties */
-    currentUrl.pathname = `${currentUrl.pathname}filter/`;
     currentUrl.searchParams.delete('page');
-    // Code for the each jQuery method is from https://api.jquery.com/each/#each-function
     $('#category-filters').find('.bg-primary').each(function () {
         categories.push($(this).text().toLowerCase().replace(' ', '_'));
     });
@@ -84,11 +68,6 @@ $('#category-filters a').on('click', function () {
     $.get(currentUrl).done(function (data) {
         $('#holidays').html(data.holidays);
         generateStars();
-        /* Code for replacing the URL pathname is from 
-        https://stackoverflow.com/questions/61896535/how-to-change-js-url-object-pathname-and-protocol-properties */
-        currentUrl.pathname = currentUrl.pathname.replace('filter/', '');
-        /*  Code for adding a new entry to the browser's session history is from
-        https://developer.mozilla.org/en-US/docs/Web/API/History/pushState */
         window.history.pushState({}, '', currentUrl);
     });
 });
@@ -97,17 +76,15 @@ $('#category-filters a').on('click', function () {
 filters is toggled on or off. An AJAX get request for the currentUrl is then submitted and the HTML
 inside the holidays ID is replaced with the HTML returned in the response and the generateStars function
 is called to generate new star ratings. A new entry is added to the browser's session history to update
-the page URL. Code for the toggleClass jQuery method is from https://api.jquery.com/toggleClass/#toggleClass1 */
+the page URL. Code for the toggleClass jQuery method is from https://api.jquery.com/toggleClass/#toggleClass1,
+code for the each jQuery method is from https://api.jquery.com/each/#each-function and code for adding a new
+entry to the  browser's session history is from https://developer.mozilla.org/en-US/docs/Web/API/History/pushState */
 
 $('#country-filters a').on('click', function () {
     $(this).find('span').toggleClass('bg-primary text-dark');
     let countries = [];
     let currentUrl = new URL(window.location);
-    /* Code for replacing the URL pathname is from 
-    https://stackoverflow.com/questions/61896535/how-to-change-js-url-object-pathname-and-protocol-properties */
-    currentUrl.pathname = `${currentUrl.pathname}filter/`;
     currentUrl.searchParams.delete('page');
-    // Code for the each jQuery method is from https://api.jquery.com/each/#each-function
     $('#country-filters').find('.bg-primary').each(function () {
         countries.push($(this).text().toLowerCase().replace(' ', '_'));
     });
@@ -120,11 +97,6 @@ $('#country-filters a').on('click', function () {
     $.get(currentUrl).done(function (data) {
         $('#holidays').html(data.holidays);
         generateStars();
-        /* Code for replacing the URL pathname is from 
-        https://stackoverflow.com/questions/61896535/how-to-change-js-url-object-pathname-and-protocol-properties */
-        currentUrl.pathname = currentUrl.pathname.replace('filter/', '');
-        /*  Code for adding a new entry to the browser's session history is from
-        https://developer.mozilla.org/en-US/docs/Web/API/History/pushState */
         window.history.pushState({}, '', currentUrl);
     });
 });
