005814fce850a9f06701fbeefec24f28400e44a1
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-06-03 16:17:05 +0100

    Removed redundant code and amended comments in flights.js

--- static/js/flights.js
+++ static/js/flights.js
@@ -1,13 +1,12 @@
 /* Updates the currentUrl pathname and the sort, direction and page search perameters when the
 value of the flight-sort-selector ID changes. An AJAX get request for the currentUrl is then
 submitted and the HTML inside the flights-table ID is replaced with the HTML returned. A new
-entry is added to the browser's session history to update the page URL. */
+entry is added to the browser's session history to update the page URL. Code for adding a new entry 
+to the browser's session history is from 
+https://developer.mozilla.org/en-US/docs/Web/API/History/pushState */
 $('#flight-sort-selector').change(function () {
     let sortSelector = $(this).val();
     let currentUrl = new URL(window.location);
-    /* Code for replacing the URL pathname is from 
-    https://stackoverflow.com/questions/61896535/how-to-change-js-url-object-pathname-and-protocol-properties */
-    currentUrl.pathname = '/flights/filter/';
     currentUrl.searchParams.delete("page");
     if (sortSelector !== "reset") {
         let sort = sortSelector.split("_")[0];
@@ -22,11 +21,6 @@ $('#flight-sort-selector').change(function () {
     }
     $.get(currentUrl).done(function (data) {
         $('#flights-table').html(data.flights);
-        /* Code for replacing the URL pathname is from 
-        https://stackoverflow.com/questions/61896535/how-to-change-js-url-object-pathname-and-protocol-properties */
-        currentUrl.pathname = '/flights/';
-        /*  Code for adding a new entry to the browser's session history is from
-        https://developer.mozilla.org/en-US/docs/Web/API/History/pushState */
         window.history.pushState({}, '', currentUrl);
     });
 });
@@ -35,21 +29,15 @@ $('#flight-sort-selector').change(function () {
 An AJAX get request for the currentUrl is then submitted and the HTML inside the flights-table
 ID is replaced with the HTML returned. A new entry is added to the browser's session history 
 to update the page URL. Code for the delegate target jQuery is from 
-https://api.jquery.com/event.delegateTarget/#event-delegateTarget1 */
+https://api.jquery.com/event.delegateTarget/#event-delegateTarget1 and code for adding a new entry
+to the browser's session history is from 
+https://developer.mozilla.org/en-US/docs/Web/API/History/pushState */
 $('#flights-table').on('click', '.page-link', function () {
     let page = $(this).data('page');
     let currentUrl = new URL(window.location);
-    /* Code for replacing the URL pathname is from 
-    https://stackoverflow.com/questions/61896535/how-to-change-js-url-object-pathname-and-protocol-properties */
-    currentUrl.pathname = '/flights/filter/';
     currentUrl.searchParams.set("page", page);
     $.get(currentUrl).done(function (data) {
         $('#flights-table').html(data.flights);
-        /* Code for replacing the URL pathname is from 
-        https://stackoverflow.com/questions/61896535/how-to-change-js-url-object-pathname-and-protocol-properties */
-        currentUrl.pathname = '/flights/';
-        /*  Code for adding a new entry to the browser's session history is from
-        https://developer.mozilla.org/en-US/docs/Web/API/History/pushState */
         window.history.pushState({}, '', currentUrl);
     });
 });
