bb911eafddac32a36948d557aa4546a7c7e4e6e1
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-06-03 16:58:31 +0100

    Amended next day template tag

--- booking/templatetags/booking_tools.py
+++ booking/templatetags/booking_tools.py
@@ -1,5 +1,4 @@
 from django import template
-from pytz import timezone
 
 
 register = template.Library()
@@ -15,26 +14,18 @@ def duration(td):
     return f'{hours}h {minutes}m'
 
 
-@register.simple_tag(name='next_day')
-def next_day(flight, time_zone, direction):
-    # https://www.w3resource.com/python-exercises/python-basic-exercise-14.php
-    # https://pypi.org/project/pytz/
-    # https://docs.djangoproject.com/en/3.2/howto/custom-template-tags/#writing-custom-template-filters
-
-    time_zone = timezone(time_zone)
-    departure = flight.departure_time
-    arrival = flight.arrival_time
-
-    if direction == 'outbound':
-        arrival = arrival.astimezone(time_zone)
-    
-    else:
-        departure = departure.astimezone(time_zone)
-
+@register.filter(name='next_day')
+def next_day(flight):
+    departure = flight.departure_time.astimezone(flight.origin_time_zone)
+    arrival = flight.arrival_time.astimezone(flight.destination_time_zone)
     difference = arrival.date() - departure.date()
 
-    if difference.days > 0:
+    if difference.days == 1:
         return f'(+ {difference.days} day)'
+
+    if difference.days > 1:
+        return f'(+ {difference.days} days)'
+
 
 @register.filter(name='extra_quantity')
 def extra_quantity(extras, item_id):
