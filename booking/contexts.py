f9752ed7c580157ab1ec0b70bd07dc3a6e8f3830
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-06-03 16:28:16 +0100

    Removed redundant code from the context processor

--- booking/contexts.py
+++ booking/contexts.py
@@ -7,12 +7,10 @@ def booking_details(request):
 
     booking_number = request.session.get('booking_number', '')
     booking = None
-    time_zone = None
     selected_extras = []
 
     if booking_number:
         booking = get_object_or_404(Booking, booking_number=booking_number)
-        time_zone = booking.outbound_flight.destination_time_zone.zone
         if booking.booking_extras.all():
             for extra in booking.booking_extras.all():
                 selected_extras.append(extra.extra.id)
@@ -24,7 +22,6 @@ def booking_details(request):
         'promo_code': settings.PROMO_CODE,
         'booking': booking,
         'selected_extras': selected_extras,
-        'time_zone': time_zone,
     }
 
     return context
