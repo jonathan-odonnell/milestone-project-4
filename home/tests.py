ccaf88ebb29e31aa4765b080ea0f4795824ba284
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-06-03 19:21:28 +0100

    Added home tests

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,13 @@
+from django.http import response
 from django.test import TestCase
+from .models import NewsletterSignUp
 
-# Create your tests here.
+class TestHomeForm(TestCase):
+    def test_get_home_page(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/index.html')
+
+    def test_item_string_method_returns_email(self):
+        email = NewsletterSignUp(email='test@example.com')
+        self.assertEqual(str(email), 'test@example.com')
