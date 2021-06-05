8ff6199ae00acc0973f927d8bffa045506ba7b65
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-06-05 20:05:03 +0100

    Amended review form to include package full name fields and updated template to not render them

--- holidays/forms.py
+++ holidays/forms.py
@@ -74,7 +74,7 @@ class ItineraryForm(forms.ModelForm):
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
-        exclude = ('package', 'full_name', 'date',)
+        exclude = ('date',)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
