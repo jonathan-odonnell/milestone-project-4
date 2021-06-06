899371de775f9726479aba8abd574eb011f37ba0
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-06-06 16:05:41 +0100

    Added to home tests

--- home/tests.py
+++ home/tests.py
@@ -2,12 +2,19 @@ from django.http import response
 from django.test import TestCase
 from .models import NewsletterSignUp
 
-class TestHomeForm(TestCase):
+class TestHomeViews(TestCase):
     def test_get_home_page(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home/index.html')
 
-    def test_item_string_method_returns_email(self):
+    def test_can_add_newsletter_signup(self):
+        response = self.client.post('/', {
+           ' email': 'test@example.com'
+        })
+        self.assertEqual(response.status_code, 200)
+
+class TestHomeModels(TestCase):
+    def test_newsletter_signup_string_method_returns_email(self):
         email = NewsletterSignUp(email='test@example.com')
         self.assertEqual(str(email), 'test@example.com')
