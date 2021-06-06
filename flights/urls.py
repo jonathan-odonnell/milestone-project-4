b13ed4eff632201ace19eef8404f91121b292d1d
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-06-06 16:53:25 +0100

    Amended the airports url

--- flights/urls.py
+++ flights/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.flights, name='flights'),
-    path('airports//<int:holiday_id>/', views.airports, name='airports'),
+    path('airports/<int:holiday_id>/', views.airports, name='airports'),
     path('add/', views.add_flight, name='add_flight'),
     path('edit/<flight_number>/', views.edit_flight, name='edit_flight'),
     path('delete/<flight_number>/', views.delete_flight, name='delete_flight'),
