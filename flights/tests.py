3104644ea65bd6067cba06b91cd280b227373cc1
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-06-06 16:53:36 +0100

    Amended the flights testing

--- flights/tests.py
+++ flights/tests.py
@@ -27,10 +27,43 @@ class TestFlightsViews(TestCase):
             password='Password',
         )
 
+        self.flight = Flight.objects.create(
+            flight_number='ZZ001',
+            direction='Outbound',
+            origin='Test Airport',
+            destination='Test Airport',
+            departure_time=datetime(2021, 6, 1, 12, tzinfo=pytz.utc),
+            origin_time_zone=timezone('Europe/London'),
+            arrival_time=datetime(2021, 6, 1, 16, tzinfo=pytz.utc),
+            destination_time_zone=timezone('America/Toronto'),
+            baggage=20
+        )
+
+    def test_get_airports_json(self):
+        self.flight.packages.create(
+            name='Test Holiday',
+            image='test_image.jpg',
+            description='Test description',
+            offer=True,
+            price=499,
+            duration=14,
+            catering='Full Board',
+            transfers_included=True
+        )
+        response = self.client.get(f'/flights/airports/{self.flight.packages.first().id}/')
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(response.content, {'airports': ['Test Airport']})
+
     def test_get_flights_page(self):
         response = self.client.get('/flights/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'flights/flights.html')
+
+    def test_get_sorted_flights_page(self):
+        # https://stackoverflow.com/questions/4794457/unit-testing-django-json-view
+        response = self.client.get('/flights/?sort=number&direction=desc',  HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'flights/includes/flights_table.html')
 
     def test_get_add_flight_page(self):
         response = self.client.get('/flights/add/')
@@ -39,7 +72,7 @@ class TestFlightsViews(TestCase):
 
     def test_can_add_flight(self):
         response = self.client.post(f'/flights/add/', {
-            'flight_number': 'ZZ001',
+            'flight_number': 'ZZ002',
             'direction': 'Outbound',
             'origin': 'Test Airport',
             'destination': 'Test Airport',
@@ -50,38 +83,16 @@ class TestFlightsViews(TestCase):
             'baggage': '20',
         })
         self.assertRedirects(response, '/flights/')
-        flight = Flight.objects.filter(flight_number='ZZ001')
+        flight = Flight.objects.filter(flight_number='ZZ002')
         self.assertEqual(len(flight), 1)
 
     def test_get_edit_flight_page(self):
-        flight = Flight.objects.create(
-            flight_number='ZZ001',
-            direction='Outbound',
-            origin='Test Airport',
-            destination='Test Airport',
-            departure_time=datetime(2021, 6, 1, 12, tzinfo=pytz.utc),
-            origin_time_zone=timezone('Europe/London'),
-            arrival_time=datetime(2021, 6, 1, 16, tzinfo=pytz.utc),
-            destination_time_zone=timezone('America/Toronto'),
-            baggage=20
-        )
-        response = self.client.get(f'/flights/edit/{flight.flight_number}/')
+        response = self.client.get(f'/flights/edit/{self.flight.flight_number}/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'flights/edit_flight.html')
 
     def test_can_edit_flight(self):
-        flight = Flight.objects.create(
-            flight_number='ZZ001',
-            direction='Outbound',
-            origin='Test Airport',
-            destination='Test Airport',
-            departure_time=datetime(2021, 6, 1, 12, tzinfo=pytz.utc),
-            origin_time_zone=timezone('Europe/London'),
-            arrival_time=datetime(2021, 6, 1, 16, tzinfo=pytz.utc),
-            destination_time_zone=timezone('America/Toronto'),
-            baggage=20
-        )
-        response = self.client.post(f'/flights/edit/{flight.flight_number}/', {
+        response = self.client.post(f'/flights/edit/{self.flight.flight_number}/', {
             'flight_number': 'ZZ001',
             'direction': 'Outbound',
             'origin': 'Test Airport',
@@ -97,18 +108,7 @@ class TestFlightsViews(TestCase):
         self.assertEqual(flight.destination, 'Test Airport 2')
 
     def test_can_delete_flight(self):
-        flight = Flight.objects.create(
-            flight_number='ZZ001',
-            direction='Outbound',
-            origin='Test Airport',
-            destination='Test Airport',
-            departure_time=datetime(2021, 6, 1, 12, tzinfo=pytz.utc),
-            origin_time_zone=timezone('Europe/London'),
-            arrival_time=datetime(2021, 6, 1, 16, tzinfo=pytz.utc),
-            destination_time_zone=timezone('America/Toronto'),
-            baggage=20
-        )
-        response = self.client.get(f'/flights/delete/{flight.flight_number}/')
+        response = self.client.get(f'/flights/delete/{self.flight.flight_number}/')
         self.assertRedirects(response, '/flights/')
         flight = Flight.objects.filter(flight_number='ZZ001')
         self.assertEqual(len(flight), 0)
@@ -191,7 +191,7 @@ class TestFlightsForm(TestCase):
 
 
 class TestFlightsModel(TestCase):
-    def test_string_method_returns_flight_number(self):
+    def test_filght_string_method_returns_flight_number(self):
         flight = Flight.objects.create(
             flight_number='ZZ001',
             direction='Outbound',
