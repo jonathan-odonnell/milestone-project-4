d36b15a5d81932390e9d3b779438102b703fb781
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-06-06 23:45:56 +0100

    Amended booking form tests

--- booking/test_forms.py
+++ booking/test_forms.py
@@ -1,38 +1,50 @@
 from django.test import TestCase
-from .forms import PassengerForm
+from .forms import PassengerForm, PassengersFormSet
 
 
 class TestBookingForm(TestCase):
     def test_all_form_fields_required(self):
-        form = PassengerForm({
-            'full_name': '',
-            'date_of_birth': '',
-            'passport_number': '',
-        })
-        self.assertEqual(form.errors['full_name']
+        form = PassengersFormSet({
+            'booking_passengers-TOTAL_FORMS': '1',
+            'booking_passengers-INITIAL_FORMS': '1',
+            'booking_passengers-MIN_NUM_FORMS': '1',
+            'booking_passengers-MAX_NUM_FORMS': '1000',
+            'booking_passengers-0-full_name': '',
+            'booking_passengers-0-date_of_birth': '',
+            'booking_passengers-0-passport_number': '',
+        }, extra=0, min_num=1)
+        self.assertEqual(form.errors[0]['full_name']
                          [0], 'This field is required.')
-        self.assertEqual(form.errors['date_of_birth']
+        self.assertEqual(form.errors[0]['date_of_birth']
                          [0], 'This field is required.')
-        self.assertEqual(form.errors['passport_number']
-                         [0], 'This field is required.')
+        self.assertEqual(
+            form.errors[0]['passport_number'][0], 'This field is required.')
 
     def test_invalid_passport_number_input(self):
-        form = PassengerForm({
-            'full_name': 'Test User',
-            'date_of_birth': '01/01/1990',
-            'passport_number': 'aaaaaaaaaa',
-        })
-        self.assertEqual(form.errors['passport_number']
-                         [0], 'Enter a number.')
+        form = PassengersFormSet({
+            'booking_passengers-TOTAL_FORMS': '1',
+            'booking_passengers-INITIAL_FORMS': '1',
+            'booking_passengers-MIN_NUM_FORMS': '1',
+            'booking_passengers-MAX_NUM_FORMS': '1000',
+            'booking_passengers-0-full_name': 'Test User',
+            'booking_passengers-0-date_of_birth': '01/01/1990',
+            'booking_passengers-0-passport_number': 'aaaaaaaaaa',
+        }, extra=0, min_num=1)
+        self.assertEqual(
+            form.errors[0]['passport_number'][0], 'Enter a number.')
 
     def test_passport_number_input_too_long(self):
-        form = PassengerForm({
-            'full_name': 'Test User',
-            'date_of_birth': '01/01/1990',
-            'passport_number': '1234567890',
-        })
-        self.assertEqual(form.errors['passport_number']
-                         [0], 'Ensure that there are no more than 9 digits in total.')
+        form = PassengersFormSet({
+            'booking_passengers-TOTAL_FORMS': '1',
+            'booking_passengers-INITIAL_FORMS': '1',
+            'booking_passengers-MIN_NUM_FORMS': '1',
+            'booking_passengers-MAX_NUM_FORMS': '1000',
+            'booking_passengers-0-full_name': 'Test User',
+            'booking_passengers-0-date_of_birth': '01/01/1990',
+            'booking_passengers-0-passport_number': '1234567890',
+        }, extra=0, min_num=1)
+        self.assertEqual(form.errors[0]['passport_number'][0],
+                         'Ensure that there are no more than 9 digits in total.')
 
     def test_fields_in_form_metaclass(self):
         form = PassengerForm()
