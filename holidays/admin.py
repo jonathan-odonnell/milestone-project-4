82f6232d47941306322d8887bd6f45ebd6e15683
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-06-08 20:51:47 +0100

    Amended holidays admin view and added comments

--- holidays/admin.py
+++ holidays/admin.py
@@ -1,38 +1,62 @@
 from django.contrib import admin
-from .models import Activity, Category, Country, Feature, Itinerary, Package, Region, Review
-from flights.models import Flight
-from extras.models import Extra
+from .models import (Activity, Category, Country, Feature,
+                     Itinerary, Package, Region, Review)
 
 
 class ActivityInline(admin.StackedInline):
+    """
+    Code for the stacked inline, extra and classes is from
+    https://docs.djangoproject.com/en/3.2/ref/contrib/admin/#inlinemodeladmin-objects
+    """
     model = Activity
     extra = 1
+    classes = ['collapse']
 
 
 class FeatureInline(admin.StackedInline):
+    """
+    Code for the stacked inline, extra and classes is from
+    https://docs.djangoproject.com/en/3.2/ref/contrib/admin/#inlinemodeladmin-objects
+    """
     model = Feature
     extra = 1
+    classes = ['collapse']
 
 
 class ItineraryInline(admin.StackedInline):
+    """
+    Code for the stacked inline, extra and classes is from
+    https://docs.djangoproject.com/en/3.2/ref/contrib/admin/#inlinemodeladmin-objects
+    """
     model = Itinerary
     extra = 1
+    classes = ['collapse']
+
 
 class ReviewInline(admin.StackedInline):
+    """
+    Code for the stacked inline, extra and classes is from
+    https://docs.djangoproject.com/en/3.2/ref/contrib/admin/#inlinemodeladmin-objects
+    """
     model = Review
     extra = 1
     fields = ('full_name', 'rating', 'title', 'review',)
+    classes = ['collapse']
 
 
 class PackageAdmin(admin.ModelAdmin):
+    """
+    Code for filter_horizontal, list_filter and list_per_page is from
+    https://docs.djangoproject.com/en/3.2/ref/contrib/admin/#modeladmin-options
+    """
     readonly_fields = ('rating',)
 
     fields = ('name', 'image', 'image_url',
               'description', 'category', 'country',
               'region', 'offer', 'price', 'duration',
-              'rating', 'catering', 'transfers_included', 
+              'rating', 'catering', 'transfers_included',
               'extras', 'flights',)
-    
+
     inlines = [
         FeatureInline,
         ActivityInline,
@@ -41,16 +65,18 @@ class PackageAdmin(admin.ModelAdmin):
     ]
 
     filter_horizontal = ('extras', 'flights')
-    
+
     list_display = ('name', 'country', 'category',
                     'price',)
+
+    list_filter = ('category', 'country')
 
     ordering = ('country', 'name')
 
     list_per_page = 20
 
 
-admin.site.register(Package, PackageAdmin)
 admin.site.register(Category)
 admin.site.register(Country)
+admin.site.register(Package, PackageAdmin)
 admin.site.register(Region)
