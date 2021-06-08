f5a9703d8b56325e3cb7db3795c9fcbf4137b3ee
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-06-08 20:17:00 +0100

    Amended home admin view to hide the sidebar and amend the site header and added comments

--- home/admin.py
+++ home/admin.py
@@ -3,8 +3,17 @@ from .models import NewsletterSignUp
 
 
 class NewsletterSignUpAdmin(admin.ModelAdmin):
-
+    """
+    Code for list_per_page is from
+    https://docs.djangoproject.com/en/3.2/ref/contrib/admin/#modeladmin-options
+    """
     list_per_page = 20
 
 
+"""
+Code to hide the sidebar and change the site header text is from
+https://docs.djangoproject.com/en/3.2/ref/contrib/admin/#adminsite-attributes
+"""
+admin.AdminSite.enable_nav_sidebar = False
+admin.AdminSite.site_header = 'Go Explore Admin'
 admin.site.register(NewsletterSignUp, NewsletterSignUpAdmin)
