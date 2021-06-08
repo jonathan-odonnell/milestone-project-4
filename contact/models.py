98a29f91c1b90b3cc78c27366768391f5f875146
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-06-08 23:54:40 +0100

    Refactored code and added comments in contact model

--- contact/models.py
+++ contact/models.py
@@ -2,20 +2,23 @@ from django.db import models
 
 
 class CustomerContact(models.Model):
-
-    SUBJECTS = [
-            ('', 'Subject'),
-            ('Holiday Information', 'Holiday Information'),
-            ('Offers', 'Offers'),
-            ('Bookings', 'Bookings'),
-            ('General Enquiries', 'General Enquiries'),
-            ('Other', 'Other')
-        ]
+    """
+    Code for direction choices is from
+    https://docs.djangoproject.com/en/3.2/ref/models/fields/#choices
+    """
+    SUBJECT_CHOICES = [
+        ('', 'Subject'),
+        ('Holiday Information', 'Holiday Information'),
+        ('Offers', 'Offers'),
+        ('Bookings', 'Bookings'),
+        ('General Enquiries', 'General Enquiries'),
+        ('Other', 'Other')
+    ]
 
     full_name = models.CharField(max_length=254)
     email = models.EmailField(max_length=254)
     date = models.DateTimeField(auto_now_add=True)
-    subject = models.CharField(max_length=254, choices=SUBJECTS)
+    subject = models.CharField(max_length=254, choices=SUBJECT_CHOICES)
     message = models.TextField()
 
     def __str__(self):
