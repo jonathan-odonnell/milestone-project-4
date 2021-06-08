7f38700b2ad178309c7ef06d2bad4807ac8a7d98
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-06-08 23:41:49 +0100

    Amended slug field, added save method and amended comments in extra model

--- extras/models.py
+++ extras/models.py
@@ -9,14 +9,22 @@ class Extra(models.Model):
     """
 
     class Meta:
+        """
+        Code for default ordering is from
+        https://docs.djangoproject.com/en/3.2/ref/models/options/#ordering
+        """
         ordering = ('id',)
 
     name = models.CharField(max_length=254)
+    slug = AutoSlugField(populate_from='name', unique=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField()
     image_url = models.CharField(max_length=254, null=True, blank=True)
-    slug = AutoSlugField(populate_from=['name'])
+
+    def save(self, *args, **kwargs):
+        self.slug = None
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f'{self.name} - £{self.price}'
