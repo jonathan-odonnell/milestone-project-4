dac153b2cfd44ecf03e2d2e019d511aa4976c4dc
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-06-09 21:41:58 +0100

    Added comments to contact admin view

--- contact/admin.py
+++ contact/admin.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
 from .models import CustomerContact
 
-class CustomerContactAdmin(admin.ModelAdmin):
 
+class CustomerContactAdmin(admin.ModelAdmin):
+    """
+    Code for list_per_page is from
+    https://docs.djangoproject.com/en/3.2/ref/contrib/admin/#modeladmin-options
+    """
     list_display = ('subject', 'full_name', 'date',)
+
     readonly_fields = ('date',)
+
     fields = ('full_name', 'email', 'date', 'subject', 'message',)
+
     ordering = ('-date',)
+
     list_per_page = 20
 
 
