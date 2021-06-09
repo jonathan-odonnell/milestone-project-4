d847171d92425fee7f010bf49f7e92a60f030530
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-06-09 17:55:40 +0100

    Amended booking query and added comments in the webhook handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -24,13 +24,13 @@ class WH_Handler:
         body = render_to_string(
             'checkout/confirmation_emails/confirmation_email_body.txt',
             {'booking': booking, 'contact_email': settings.DEFAULT_FROM_EMAIL})
-        
+
         send_mail(
             subject,
             body,
             settings.DEFAULT_FROM_EMAIL,
             [cust_email]
-        )       
+        )
 
     def handle_event(self, event):
         """
@@ -55,16 +55,30 @@ class WH_Handler:
         shipping_details = intent.shipping
         total = round(intent.charges.data[0].amount / 100, 2)
 
+        """
+        Sets any shipping details in the payment intent
+        with a value of an empty string to None
+        """
         for field, value in shipping_details.address.items():
             if value == "":
                 shipping_details.address[field] = None
 
+        # Attempts to retrieve the booking every second for five seconds.
         booking_exists = False
         attempt = 1
         while attempt <= 5:
             try:
                 booking = Booking.objects.get(
                     booking_number=booking_number,
+                    full_name__iexact=shipping_details.name,
+                    email__iexact=billing_details.email,
+                    phone_number__iexact=shipping_details.phone,
+                    country__iexact=shipping_details.address.country,
+                    postcode__iexact=shipping_details.address.postal_code,
+                    town_or_city__iexact=shipping_details.address.city,
+                    street_address1__iexact=shipping_details.address.line1,
+                    street_address2__iexact=shipping_details.address.line2,
+                    county__iexact=shipping_details.address.state,
                     grand_total=total,
                     paid=True,
                     stripe_pid=pid,
@@ -76,15 +90,25 @@ class WH_Handler:
                 attempt += 1
                 time.sleep(1)
 
+        """
+        Sends the confirmation email and a success response
+        to stripe if the booking already exists
+        """
         if booking_exists:
             self.send_confirmation_email(booking)
             return HttpResponse(
-                content=f'Webhook received: {event["type"]} | SUCCESS: Verified booking already in database',
+                content=f'Webhook received: {event["type"]} | \
+                    SUCCESS: Verified booking already in database',
                 status=200)
 
         else:
             booking = None
             try:
+                """
+                If booking_exists remains false after 5 attempts, try and
+                retreive the booking and update the booking contact details
+                and payment details from the payment intent
+                """
                 booking = Booking.objects.get(booking_number=booking_number)
                 booking.full_name = shipping_details.name
                 booking.email = billing_details.email
@@ -99,6 +123,10 @@ class WH_Handler:
                 booking.stripe_pid = pid
                 booking.save()
 
+                """
+                Saves the user's details to their profile if they were signed
+                in and they checked the save_info checkbox in the form
+                """
                 if username != 'AnonymousUser':
                     profile = UserProfile.objects.get(user__username=username)
                     booking.update(user_profile=profile)
@@ -119,14 +147,21 @@ class WH_Handler:
                         if user_profile_form.is_valid():
                             user_profile_form.save()
 
+            # Sends an error response to stripe if the booking does not exist
             except Booking.DoesNotExist:
                 return HttpResponse(
-                    content=f'Webhook received: {event["type"]} | ERROR: Booking does not exist',
+                    content=f'Webhook received: {event["type"]} | \
+                        ERROR: Booking does not exist',
                     status=500)
 
+        """
+        Sends the confirmation email and a success response
+        to stripe if the booking already exists
+        """
         self.send_confirmation_email(booking)
         return HttpResponse(
-            content=f'Webhook received: {event["type"]} | SUCCESS: Created booking in webhook',
+            content=f'Webhook received: {event["type"]} | \
+                SUCCESS: Created booking in webhook',
             status=200)
 
     def handle_payment_intent_payment_failed(self, event):
@@ -136,4 +171,3 @@ class WH_Handler:
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
-
