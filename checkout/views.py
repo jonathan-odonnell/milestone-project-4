2dd4310b2bd4cf465bde3eb067acd77e338c249d
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-06-09 17:54:48 +0100

    Added comments and refactored code in the checkout views

--- checkout/views.py
+++ checkout/views.py
@@ -1,10 +1,7 @@
-from re import I
-from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
-from django.shortcuts import render, HttpResponse, redirect, reverse, get_object_or_404
+from django.shortcuts import (
+    render, HttpResponse, redirect, reverse, get_object_or_404)
 from django.contrib import messages
-from django.http import JsonResponse
-from django.template.loader import render_to_string
 from django.conf import settings
 from booking.models import Booking
 from profiles.models import UserProfile
@@ -19,6 +16,10 @@ import json
 
 @require_POST
 def cache_checkout_data(request):
+    """
+    A view to amend the payment intent metadata and add the save_info,
+    username and booking_number values.
+    """
     try:
         pid = request.POST.get('client_secret').split('_secret')[0]
         stripe.api_key = settings.STRIPE_SECRET_KEY
@@ -35,16 +36,28 @@ def cache_checkout_data(request):
 
 
 def checkout(request):
+    """
+    A view to set up the payment intent, display the checkout page and process
+    the booking form once the payment has been approved.
+    """
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
     booking_number = request.session.get('booking_number', '')
     profile = None
     cards = None
 
+    """
+    Checks that there is a valid booking number in the browser's session
+    variable and passenger details have been added to the booking.
+    """
     if not booking_number:
         return redirect(reverse('booking'))
 
-    booking = Booking.objects.get(booking_number=booking_number)
+    else:
+        booking = Booking.objects.get(booking_number=booking_number)
+
+    if not booking.booking_passengers.all():
+        return redirect(reverse('passengers'))
 
     if request.method == 'POST':
         save_info = 'save_info' in request.POST
@@ -52,6 +65,7 @@ def checkout(request):
         paypal_pid = request.POST['paypal_pid']
         stripe.api_key = settings.STRIPE_SECRET_KEY
 
+        # Saves the booking form if it is valid
         form_data = {
             'full_name': request.POST['full_name'],
             'email': request.POST['email'],
@@ -69,6 +83,7 @@ def checkout(request):
         if booking_form.is_valid():
             booking = booking_form.save(commit=False)
 
+            # Adds the relevant payment id to the booking and sets paid to true
             if paypal_pid:
                 booking.paypal_pid = paypal_pid
                 booking.paid = True
@@ -82,6 +97,10 @@ def checkout(request):
                 booking.user_profile = profile
                 booking.save()
 
+                """
+                Saves the user's details to their profile if the
+                save info checkbox box was checked in the form
+                """
                 if save_info:
                     profile_data = {
                         'email_address': profile.user.email,
@@ -102,11 +121,13 @@ def checkout(request):
             else:
                 booking.save()
 
+            # Sends the confirmation email if the payment method was paypal
             if paypal_pid:
                 handler = WH_Handler(request)
-                handler.send_confirmation_email(booking)    
+                handler.send_confirmation_email(booking)
 
-            return redirect(reverse('checkout_success', args=[booking.booking_number]))
+            return redirect(reverse('checkout_success',
+                                    args=[booking.booking_number]))
 
         else:
             messages.error(request, 'There was an error with your form. \
@@ -118,6 +139,11 @@ def checkout(request):
         stripe.api_key = stripe_secret_key
 
         if request.user.is_authenticated:
+            """
+            Creates a booking form filled in with the user's profile data
+            and a new payment intent with their customer id attached. Code
+            is from https://stripe.com/docs/payments/save-during-payment
+            """
             profile = UserProfile.objects.get(user=request.user)
             booking_form = BookingForm(initial={
                 'full_name': profile.user.get_full_name(),
@@ -138,18 +164,26 @@ def checkout(request):
             )
 
         else:
+            """
+            Creates an empty booking form and a new payment intent
+            with no customer id attached
+            """
             booking_form = BookingForm()
             intent = stripe.PaymentIntent.create(
                 amount=stripe_total,
                 currency=settings.STRIPE_CURRENCY
             )
 
+    """
+    Gets up to 3 of the customer's latest saved cards if they are logged in.
+    Code is from https://stripe.com/docs/api/cards/list
+    """
     if request.user.is_authenticated:
         cards = stripe.PaymentMethod.list(
-                customer=profile.stripe_customer_id,
-                type="card",
-                limit=3
-            )
+            customer=profile.stripe_customer_id,
+            type="card",
+            limit=3
+        )
 
     context = {
         'booking_form': booking_form,
@@ -168,7 +202,8 @@ def checkout(request):
 
 def checkout_success(request, booking_number):
     """
-    Handle successful checkouts
+    A view to handle successful checkouts and display the
+    checkout success page.
     """
     booking = get_object_or_404(Booking, booking_number=booking_number)
 
@@ -184,6 +219,12 @@ def checkout_success(request, booking_number):
 
 
 def paypal(request):
+    """
+    A view to set up a paypal transaction. Code is from
+    https://developer.paypal.com/docs/checkout/reference/server-integration/setup-sdk/
+    and
+    https://developer.paypal.com/docs/checkout/reference/server-integration/set-up-transaction/
+    """
     client_id = settings.PAYPAL_CLIENT_ID
     client_secret = settings.PAYPAL_CLIENT_SECRET
     booking_number = request.session.get('booking_number', '')
@@ -208,19 +249,22 @@ def paypal(request):
                   "value": str(total),
               }}]})
     response = client.execute(request)
-    response = response.result.__dict__['_dict']
-    return JsonResponse(response)
+    return response
 
 
 def paypal_approve(request):
+    """
+    A view to capture the paypal transaction funds. Code is from
+    https://stackoverflow.com/questions/59630300/getting-bytes-when-using-axios
+    and
+    https://developer.paypal.com/docs/checkout/reference/server-integration/capture-transaction/
+    """
     client_id = settings.PAYPAL_CLIENT_ID
     client_secret = settings.PAYPAL_CLIENT_SECRET
-    # From https://stackoverflow.com/questions/59630300/getting-bytes-when-using-axios
     order_id = json.loads(request.body)['order_id']
     environment = SandboxEnvironment(
         client_id=client_id, client_secret=client_secret)
     client = PayPalHttpClient(environment)
     request = OrdersCaptureRequest(order_id)
     response = client.execute(request)
-    response = response.result.__dict__['_dict']
-    return JsonResponse(response)
+    return response
