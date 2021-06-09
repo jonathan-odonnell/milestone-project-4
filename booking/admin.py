7997c614edd8cb53b9ffa5343bec572b535c5b77
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-06-09 23:19:14 +0100

    Added list display, ordering and items per page to coupon admin view

--- booking/admin.py
+++ booking/admin.py
@@ -2,6 +2,18 @@ from django.contrib import admin
 from .models import Booking, BookingExtra, BookingPassenger, Coupon
 
 
+class CouponAdmin(admin.ModelAdmin):
+    """
+    Code for list_per_page is from
+    https://docs.djangoproject.com/en/3.2/ref/contrib/admin/#modeladmin-options
+    """
+    list_display = ('name', 'start_date', 'end_date', 'amount',)
+
+    ordering = ('-end_date',)
+
+    list_per_page = 20
+
+
 class BookingExtraAdminInline(admin.TabularInline):
     """
     Code for the extra and classes is from
@@ -28,7 +40,6 @@ class BookingAdmin(admin.ModelAdmin):
     Code for list_per_page is from
     https://docs.djangoproject.com/en/3.2/ref/contrib/admin/#modeladmin-options
     """
-
     inlines = (BookingExtraAdminInline, BookingPassengerAdminInline)
 
     readonly_fields = ('booking_number', 'date', 'coupon', 'subtotal',
@@ -43,13 +54,12 @@ class BookingAdmin(admin.ModelAdmin):
               'extras_total', 'grand_total', 'paid', 'stripe_pid',
               'paypal_pid')
 
-    list_display = ('booking_number', 'date', 'full_name',
-                    'grand_total',)
+    list_display = ('booking_number', 'date', 'full_name', 'grand_total',)
 
     ordering = ('-date',)
 
     list_per_page = 20
 
 
-admin.site.register(Coupon)
+admin.site.register(Coupon, CouponAdmin)
 admin.site.register(Booking, BookingAdmin)
