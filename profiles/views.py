0571c4c859a0349992cf9b12f6f122dc6f673663
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-06-14 19:49:14 +0100

    Amended comments in profiles views

--- profiles/views.py
+++ profiles/views.py
@@ -6,9 +6,13 @@ from .forms import UserProfileForm
 from django.conf import settings
 import stripe
 
+
 @login_required
 def profile(request):
-    """A view to display and update the user's profile."""
+    """
+    A view to display the profile page and
+    update the user's profile in the database.
+    """
     profile = get_object_or_404(UserProfile, user=request.user)
     api_key = settings.GOOGLE_PLACES_KEY
 
@@ -23,14 +27,14 @@ def profile(request):
     else:
         form = UserProfileForm(
             initial={'email_address': profile.user.email,
-                    'phone_number': profile.phone_number,
-                    'street_address1': profile.street_address1,
-                    'street_address2': profile.street_address2,
-                    'town_or_city': profile.town_or_city,
-                    'county': profile.county,
-                    'country': profile.country,
-                    'postcode': profile.postcode,
-                    })
+                     'phone_number': profile.phone_number,
+                     'street_address1': profile.street_address1,
+                     'street_address2': profile.street_address2,
+                     'town_or_city': profile.town_or_city,
+                     'county': profile.county,
+                     'country': profile.country,
+                     'postcode': profile.postcode,
+                     })
 
     template = 'profiles/profile.html'
     context = {
@@ -40,9 +44,10 @@ def profile(request):
 
     return render(request, template, context)
 
+
 @login_required
 def bookings(request):
-    """ A view to show the user's bookings."""
+    """ A view to show the bookings page."""
     profile = get_object_or_404(UserProfile, user=request.user)
     bookings = profile.bookings.filter(paid=True)
 
@@ -53,9 +58,10 @@ def bookings(request):
 
     return render(request, template, context)
 
+
 @login_required
 def booking_details(request, booking_number):
-    """A view to show a booking's details."""
+    """A view to show the booking details page."""
     booking = get_object_or_404(Booking, booking_number=booking_number)
 
     template = 'checkout/checkout_success.html'
