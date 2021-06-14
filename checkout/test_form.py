d2ad4b6b8f575ce722e7e52a9ff7e0692ea2fc56
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-06-14 22:56:01 +0100

    Amended tests and added comments in checkout tests

--- checkout/test_form.py
+++ checkout/test_form.py
@@ -1,58 +1,10 @@
 from django.test import TestCase
-from django.contrib.auth.models import User
-from booking.models import Booking
-from holidays.models import Package
 from .forms import CheckoutForm
-from datetime import date, datetime
-from pytz import timezone
-import pytz
 
 
 class TestCheckoutForm(TestCase):
-    def setUp(self):
-
-        self.user = User.objects.create_user(
-            username='test',
-            email='test@example.com',
-            password='Password',
-        )
-
-        self.holiday = Package.objects.create(
-            name='Test Holiday',
-            image='testimage.jpg',
-            description='Test description',
-            offer=True,
-            price=499,
-            duration=14,
-            catering='Full Board',
-            transfers_included=True
-        )
-
-        """
-        Code for adding the flights related object is from
-        https://docs.djangoproject.com/en/3.2/ref/models/relations/
-        """
-        self.holiday.flights.create(
-            flight_number='ZZ001',
-            origin='Test Airport',
-            destination='Test Airport',
-            departure_time=datetime(2021, 6, 1, 12, tzinfo=pytz.utc),
-            origin_time_zone=timezone('Europe/London'),
-            arrival_time=datetime(2021, 6, 2, 18, 12, tzinfo=pytz.utc),
-            destination_time_zone=timezone('America/Toronto'),
-            baggage=20
-        )
-
-        self.booking = Booking.objects.create(
-            package=self.holiday,
-            guests=1,
-            departure_date=date(2021, 6, 1),
-            return_date=date(2021, 6, 10),
-            outbound_flight=self.holiday.flights.first(),
-            return_flight=self.holiday.flights.first(),
-        )
-
     def test_all_form_fields_required(self):
+        """Tests the required fields in the extra form."""
         form = CheckoutForm({
             'full_name': '',
             'email': '',
@@ -79,6 +31,7 @@ class TestCheckoutForm(TestCase):
                          [0], 'This field is required.')
 
     def test_invalid_email_address_field(self):
+        """Tests the email address invalid input in the extra form"""
         form = CheckoutForm({
             'full_name': 'Test User',
             'email': 'Not an email address',
@@ -93,8 +46,10 @@ class TestCheckoutForm(TestCase):
                          [0], 'Enter a valid email address.')
 
     def test_fields_in_form_metaclass(self):
+        """Tests the excluded attribute of the checkout form meta class"""
         form = CheckoutForm()
-        self.assertEqual(form.Meta.fields, ('full_name', 'email', 'phone_number',
-                                            'street_address1', 'street_address2',
-                                            'town_or_city', 'county', 'postcode',
-                                            'country',))
+        self.assertEqual(
+            form.Meta.fields, ('full_name', 'email', 'phone_number',
+                               'street_address1', 'street_address2',
+                               'town_or_city', 'county', 'postcode',
+                               'country',))
--- checkout/test_views.py
+++ checkout/test_views.py
@@ -1,5 +1,6 @@
 from django.test import RequestFactory, TestCase
 from django.contrib.auth.models import User
+from allauth.account.models import EmailAddress
 from .views import checkout, cache_checkout_data, paypal, checkout_success
 from holidays.models import Package
 from booking.models import Booking
@@ -8,17 +9,35 @@ from django.conf import settings
 from django.contrib.auth.models import AnonymousUser
 from datetime import date, datetime
 from pytz import timezone
-import pytz, stripe
+import pytz
+import stripe
 
 
 class TestCheckoutViews(TestCase):
     def setUp(self):
+        """
+        Sets up the request factory, user, email address, package, flight,
+        booking, booking passenger and payment intent. Code for the request
+        factory is from
+        https://docs.djangoproject.com/en/3.2/topics/testing/advanced/#the-request-factory,
+        code for creating the user is from
+        https://docs.djangoproject.com/en/3.2/topics/testing/advanced/,
+        code for creating the email address is from
+        https://github.com/pennersr/django-allauth/blob/master/allauth/account/models.py,
+        code for creating the flight and booking passenger is from
+        https://docs.djangoproject.com/en/3.2/ref/models/relations/
+        """
         self.factory = RequestFactory()
 
         self.user = User.objects.create_user(
             username='test',
             email='test@example.com',
             password='Password',
+        )
+
+        EmailAddress.objects.create(
+            user=self.user,
+            email=self.user.email,
         )
 
         self.holiday = Package.objects.create(
@@ -32,10 +51,6 @@ class TestCheckoutViews(TestCase):
             transfers_included=True
         )
 
-        """
-        Code for adding the flights related object is from 
-        https://docs.djangoproject.com/en/3.2/ref/models/relations/
-        """
         self.holiday.flights.create(
             flight_number='ZZ001',
             origin='Test Airport',
@@ -72,6 +87,14 @@ class TestCheckoutViews(TestCase):
         )
 
     def test_anonymous_user_get_checkout_page(self):
+        """
+        Verifies that a status of 200 is returned when the user tries to
+        access the checkout page and a booking number is stored in the
+        browser's session variable. Code for the get request, setting
+        the anonymous user and setting the booking number in the session
+        variable is from
+        https://docs.djangoproject.com/en/3.2/topics/testing/advanced/#the-request-factory
+        """
         request = self.factory.get('/checkout/')
         request.user = AnonymousUser()
         request.session = {'booking_number': self.booking.booking_number}
@@ -79,6 +102,14 @@ class TestCheckoutViews(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_logged_in_user_get_checkout_page(self):
+        """
+        Logs in the user and verifies that a status of 200 is returned
+        when the user tries to access the checkout page and a booking
+        number is stored in the browser's session variable. Code for
+        the get request, setting the logging in the user and setting
+        the booking number in the session variable is from
+        https://docs.djangoproject.com/en/3.2/topics/testing/advanced/#the-request-factory
+        """
         request = self.factory.get('/checkout/')
         request.user = self.user
         request.session = {'booking_number': self.booking.booking_number}
@@ -86,31 +117,58 @@ class TestCheckoutViews(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_get_checkout_success_page(self):
+        """
+        Verifies that a status of 200 is returned when the user tries to
+        access the checkout success page and a booking number is stored
+        in the browser's session variable. Code for the get request and
+        setting the booking number in the session variable is from
+        https://docs.djangoproject.com/en/3.2/topics/testing/advanced/#the-request-factory
+        """
         request = self.factory.get(
             f'/checkout/success/{self.booking.booking_number}/')
-        request.user = AnonymousUser()
         request.session = {'booking_number': self.booking.booking_number}
         response = checkout_success(request, self.booking.booking_number)
         self.assertEqual(response.status_code, 200)
 
     def test_cache_checkout(self):
+        """
+        Verifies that a status of 200 is returned when a post request
+        with valid client secret and save info details is submitted to the
+        cache checkout page and a booking number is stored in the browser's
+        session variable. Code for the post request and setting the
+        booking number in the session variable is from
+        https://docs.djangoproject.com/en/3.2/topics/testing/advanced/#the-request-factory
+        """
         request = self.factory.post('/checkout/cache_checkout_data/', {
             'save_info': 'true',
             'client_secret': self.intent.client_secret
         })
-        request.user = AnonymousUser()
         request.session = {'booking_number': self.booking.booking_number}
         response = cache_checkout_data(request)
         self.assertEqual(response.status_code, 200)
 
     def test_create_paypal_order(self):
+        """
+        Verifies that a status of 200 is returned when a post request is
+        submitted to the paypal page and a booking number is stored in the
+        browser's session variable. Code for the post request and setting the
+        booking number in the session variable is from
+        https://docs.djangoproject.com/en/3.2/topics/testing/advanced/#the-request-factory
+        """
         request = self.factory.post('/checkout/paypal/', {})
         request.session = {'booking_number': self.booking.booking_number}
         response = paypal(request)
         self.assertEqual(response.status_code, 200)
 
-
-    def test_submit_checkout_form_stripe(self):
+    def test_anonymous_user_submit_checkout_form_stripe(self):
+        """
+        Verifies that a status of 302 is returned and a the user's profile is
+        updated in the database when a post request with valid customer and
+        stripe payment details is submitted to the checkout page. Code for the
+        post request, setting the anonymous user and setting the booking number
+        in the session variable is from
+        https://docs.djangoproject.com/en/3.2/topics/testing/advanced/#the-request-factory
+        """
         request = self.factory.post('/checkout/', {
             'full_name': 'Test User',
             'email': 'test@example.com',
@@ -128,14 +186,65 @@ class TestCheckoutViews(TestCase):
         request.user = AnonymousUser()
         request.session = {'booking_number': self.booking.booking_number}
         response = checkout(request)
+        self.assertEqual(response.status_code, 302)
         booking = Booking.objects.get(
             booking_number=self.booking.booking_number)
-        self.assertEqual(response.status_code, 302)
+        self.assertEqual(booking.phone_number, '00000000000')
+        self.assertEqual(booking.street_address1, 'Test')
+        self.assertEqual(booking.street_address2, 'Test')
+        self.assertEqual(booking.town_or_city, 'Test')
+        self.assertEqual(booking.county, 'Test')
+        self.assertEqual(booking.country, 'GB')
+        self.assertEqual(booking.postcode, 'Test')
         self.assertTrue(booking.paid)
         self.assertEqual(booking.stripe_pid,
                          self.intent.client_secret.split('_secret')[0])
 
+    def test_logged_in_user_submit_checkout_form_stripe(self):
+        """
+        Logs in the user and verifies that a status of 302 is returned and a
+        the user's profile is updated in the database when a post request with
+        valid customer and stripe payment details is submitted to the checkout
+        page. Code for the post request, setting the user and setting the
+        booking number in the session variable is from
+        https://docs.djangoproject.com/en/3.2/topics/testing/advanced/#the-request-factory
+        """
+        request = self.factory.post('/checkout/', {
+            'full_name': 'Test User',
+            'email': 'test@example.com',
+            'phone_number': '00000000000',
+            'street_address1': 'Test',
+            'street_address2': 'Test',
+            'town_or_city': 'Test',
+            'county': 'Test',
+            'country': 'GB',
+            'postcode': 'Test',
+            'client_secret': self.intent.client_secret,
+            'paypal_pid': '',
+
+        })
+        request.user = self.user
+        request.session = {'booking_number': self.booking.booking_number}
+        response = checkout(request)
+        self.assertEqual(response.status_code, 302)
+        profile = UserProfile.objects.get(user=self.user)
+        self.assertEqual(profile.phone_number, '00000000000')
+        self.assertEqual(profile.street_address1, 'Test')
+        self.assertEqual(profile.street_address2, 'Test')
+        self.assertEqual(profile.town_or_city, 'Test')
+        self.assertEqual(profile.county, 'Test')
+        self.assertEqual(profile.country, 'GB')
+        self.assertEqual(profile.postcode, 'Test')
+
     def test_submit_checkout_form_paypal(self):
+        """
+        Verifies that a status of 302 is returned and a the booking is updated
+        in the database when a post request with valid customer and paypal
+        payment details is submitted to the checkout page. Code for the post
+        request, setting the anonymous user and setting the booking number in
+        the session variable is from
+        https://docs.djangoproject.com/en/3.2/topics/testing/advanced/#the-request-factory
+        """
         request = self.factory.post('/checkout/', {
             'full_name': 'Test User',
             'email': 'test@example.com',
@@ -151,19 +260,18 @@ class TestCheckoutViews(TestCase):
             'paypal_pid': '1A5H6J8L9N6H5D',
 
         })
-        request.user = self.user
+        request.user = AnonymousUser()
         request.session = {'booking_number': self.booking.booking_number}
         response = checkout(request)
+        self.assertEqual(response.status_code, 302)
         booking = Booking.objects.get(
             booking_number=self.booking.booking_number)
-        self.assertEqual(response.status_code, 302)
+        self.assertEqual(booking.phone_number, '00000000000')
+        self.assertEqual(booking.street_address1, 'Test')
+        self.assertEqual(booking.street_address2, 'Test')
+        self.assertEqual(booking.town_or_city, 'Test')
+        self.assertEqual(booking.county, 'Test')
+        self.assertEqual(booking.country, 'GB')
+        self.assertEqual(booking.postcode, 'Test')
         self.assertTrue(booking.paid)
         self.assertEqual(booking.paypal_pid, '1A5H6J8L9N6H5D')
-        profile = UserProfile.objects.get(user=self.user)
-        self.assertEqual(profile.phone_number, '00000000000')
-        self.assertEqual(profile.street_address1, 'Test')
-        self.assertEqual(profile.street_address2, 'Test')
-        self.assertEqual(profile.town_or_city, 'Test')
-        self.assertEqual(profile.county, 'Test')
-        self.assertEqual(profile.country, 'GB')
-        self.assertEqual(profile.postcode, 'Test')
