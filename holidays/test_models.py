e7ebdeab207367a7efa1ceea2d37cf785b3f93f1
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-06-15 17:12:22 +0100

    Added comments to holidays models tests

--- holidays/test_models.py
+++ holidays/test_models.py
@@ -1,11 +1,15 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from .models import(Category, Region, Country, Package,
-                    Feature, Activity, Itinerary, Review)
+from .models import (Category, Region, Country, Package,
+                     Feature, Activity, Itinerary, Review)
 
 
 class TestHolidaysModels(TestCase):
     def setUp(self):
+        """
+        Sets up the user and package. Code for creating the user is from
+        https://docs.djangoproject.com/en/3.2/topics/testing/advanced/
+        """
         self.user = User.objects.create_superuser(
             username='test',
             email='test@example.com',
@@ -13,7 +17,7 @@ class TestHolidaysModels(TestCase):
             first_name='Test',
             last_name='User',
         )
-       
+
         self.holiday = Package.objects.create(
             name='Test Holiday',
             image='test_image.jpg',
@@ -34,6 +38,7 @@ class TestHolidaysModels(TestCase):
         self.assertEqual(str(category), category.name)
 
     def test_region_string_method(self):
+        """Verifies that the region string method is correct"""
         region = Region.objects.create(
             name="North America",
             page_title="North America",
@@ -42,18 +47,22 @@ class TestHolidaysModels(TestCase):
         self.assertEqual(str(region), region.name)
 
     def test_country_string_method(self):
+        """Verifies that the country string method is correct"""
         country = Country.objects.create(
             name='USA'
         )
         self.assertEqual(str(country), country.name)
 
     def test_package_string_method(self):
+        """Verifies that the package string method is correct"""
         self.assertEqual(str(self.holiday), self.holiday.name)
 
     def test_package_rating_default(self):
+        """Verifies that the defaults on the package model are correct"""
         self.assertEqual(self.holiday.rating, 0)
 
     def test_feature_string_method(self):
+        """Verifies that the feature string method is correct"""
         feature = Feature.objects.create(
             package=self.holiday,
             name='Test Feature'
@@ -61,6 +70,7 @@ class TestHolidaysModels(TestCase):
         self.assertEqual(str(feature), feature.name)
 
     def test_activity_string_method(self):
+        """Verifies that the activity string method is correct"""
         activity = Activity.objects.create(
             package=self.holiday,
             name='Test Activity',
@@ -69,6 +79,7 @@ class TestHolidaysModels(TestCase):
         self.assertEqual(str(activity), activity.name)
 
     def test_itinerary_string_method(self):
+        """Verifies that the itinerary string method is correct"""
         itinerary = Itinerary.objects.create(
             package=self.holiday,
             day='Day 1',
@@ -78,6 +89,7 @@ class TestHolidaysModels(TestCase):
         self.assertEqual(str(itinerary), itinerary.name)
 
     def test_review_string_method(self):
+        """Verifies that the review string method is correct"""
         review = Review.objects.create(
             package=self.holiday,
             full_name=self.user.get_full_name(),
