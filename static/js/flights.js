1d541817cd15ce98acb2019d6055f84251dce7f0
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-07-15 15:47:29 +0100

    Updated flights javascript to submit ajax requests to the filter flights url

--- static/js/flights.js
+++ static/js/flights.js
@@ -1,9 +1,12 @@
-/* Updates the currentUrl pathname and the sort, direction and page search perameters when the
-value of the flight-sort-selector ID changes. An AJAX get request for the currentUrl is then
-submitted and the HTML inside the flights-table ID is replaced with the HTML returned. A new
-entry is added to the browser's session history to update the page URL. Code for adding a new entry 
-to the browser's session history is from 
+/* Updates the sort, direction and page search perameters when the value of the 
+light-sort-selector ID changes and and replaces the currentUrl pathname to get the
+filterUrl. An AJAX get request for the filterUrl is then submitted and the HTML inside
+the flights-table ID is replaced with the HTML returned. A new entry is added to the
+browser's session history to update the page URL. Code for updating the URL's pathname
+is from https://developer.mozilla.org/en-US/docs/Web/API/URL/pathname 
+and code for adding a new entry to the  browser's session history is from
 https://developer.mozilla.org/en-US/docs/Web/API/History/pushState */
+
 $('#flight-sort-selector').change(function () {
     let sortSelector = $(this).val();
     let currentUrl = new URL(window.location);
@@ -18,25 +21,31 @@ $('#flight-sort-selector').change(function () {
     } else {
         currentUrl.searchParams.delete("sort");
         currentUrl.searchParams.delete("direction");
-    }
-    $.get(currentUrl).done(function (data) {
+    };
+    let filterUrl = new URL(currentUrl)
+    filterUrl.pathname = filterUrl.pathname.replace('flights', 'flights/filter')
+    $.get(filterUrl).done(function (data) {
         $('#flights-table').html(data.flights);
         window.history.pushState({}, '', currentUrl);
     });
 });
 
-/* Updates the currentUrl pathname and page search perameter when a page link is clicked. 
-An AJAX get request for the currentUrl is then submitted and the HTML inside the flights-table
-ID is replaced with the HTML returned. A new entry is added to the browser's session history 
-to update the page URL. Code for the delegate target jQuery is from 
-https://api.jquery.com/event.delegateTarget/#event-delegateTarget1 and code for adding a new entry
-to the browser's session history is from 
+/* Updates the cpage search perameter when a page link is clicked and replaces the currentUrl
+pathname to get the filterUrl. An AJAX get request for the filterUrl is then submitted and the
+HTML inside the flights-table ID is replaced with the HTML returned. A new entry is added to the
+browser's session history to update the page's URL. Code for the delegate target jQuery is from 
+https://api.jquery.com/event.delegateTarget/#event-delegateTarget1, code for
+updating the URL's pathname is from https://developer.mozilla.org/en-US/docs/Web/API/URL/pathname 
+and code for adding a new entry to the  browser's session history is from
 https://developer.mozilla.org/en-US/docs/Web/API/History/pushState */
+
 $('#flights-table').on('click', '.page-link', function () {
     let page = $(this).data('page');
     let currentUrl = new URL(window.location);
     currentUrl.searchParams.set("page", page);
-    $.get(currentUrl).done(function (data) {
+    let filterUrl = new URL(currentUrl)
+    filterUrl.pathname = filterUrl.pathname.replace('flights', 'flights/filter')
+    $.get(filterUrl).done(function (data) {
         $('#flights-table').html(data.flights);
         window.history.pushState({}, '', currentUrl);
     });
