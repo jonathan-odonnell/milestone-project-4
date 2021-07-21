0eb6ddc6144e923da80ed3ec24b2361c5af2472e
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-07-21 16:55:07 +0100

    Refactored code in flights_form.js

--- static/js/flights_form.js
+++ static/js/flights_form.js
@@ -1,4 +1,4 @@
-/* /* Sets the start date and end date as the startDate and endDate variables.
+/* Sets the start date and end date as the startDate and endDate variables.
 Code is from https://gijgo.com/datepicker/configuration/minDate and 
 https://gijgo.com/datepicker/configuration/maxDate */
 
@@ -12,17 +12,6 @@ let endDate = new Date(
 	new Date().getMonth(),
 	new Date().getDate()
 );
-
-/* Removes the default datepicker input styling from the departure time and arrival time
-inputs when the DOM has finished loading */
-$(document).ready(function() {
-	$("#div_id_departure_time input").removeClass(
-		"gj-textbox-md"
-	);
-	$("#div_id_arrival_time input").removeClass(
-		"gj-textbox-md"
-	);
-})
 
 /* Configures the departure time and arrival time datetime pickers. Code is from 
 https://gijgo.com/datetimepicker/, https://gijgo.com/datepicker/configuration and
@@ -49,6 +38,16 @@ $('#id_arrival_time').datetimepicker({
 	},
 	format: 'dd/mm/yyyy HH:MM',
 });
+
+/* Removes the default datepicker input styling from the departure time and arrival time
+inputs when the DOM has finished loading */
+
+$("#div_id_departure_time input").removeClass(
+	"gj-textbox-md"
+);
+$("#div_id_arrival_time input").removeClass(
+	"gj-textbox-md"
+);
 
 // Prevents the user from being able to enter text in the departure time or arrival time fields.
 
