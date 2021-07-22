e3156aa6b9fd332c46182e2dc68f7905bf831644
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-07-22 22:01:09 +0100

    Added comments to widgets.py

--- holidays/widgets.py
+++ holidays/widgets.py
@@ -1,8 +1,13 @@
 from django.forms.widgets import ClearableFileInput
 from django.utils.translation import gettext_lazy as _
 
+
 class CustomClearableFileInput(ClearableFileInput):
+    """
+    Overrides clearable file input default values
+    """
     clear_checkbox_label = _('Remove')
     initial_text = _('Current Image')
     input_text = _('')
-    template_name = 'holidays/custom_widget_templates/custom_clearable_file_input.html'
+    template_name = ('holidays/custom_widget_templates/'
+                     + 'custom_clearable_file_input.html')
