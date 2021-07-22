19c07b31e07fade11b512377b90a3288c6dbc821
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-07-22 22:01:25 +0100

    Added comments to custom storages.py

--- custom_storages.py
+++ custom_storages.py
@@ -3,8 +3,14 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticStorage(S3Boto3Storage):
+    """
+    Sets the static files storage location
+    """
     location = settings.STATICFILES_LOCATION
 
 
 class MediaStorage(S3Boto3Storage):
+    """
+    Sets the media files storage location
+    """
     location = settings.MEDIAFILES_LOCATION
