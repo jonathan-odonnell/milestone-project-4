f36da2bed17a278ea3f88aa24b18d0200ecb9b2b
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-07-22 21:39:34 +0100

    Amended and added comments to superuser required wrap

--- holidays/utlis.py
+++ holidays/utlis.py
@@ -1,10 +1,16 @@
 from functools import wraps
-from django.shortcuts import HttpResponse
+from django.core.exceptions import PermissionDenied
+
 
 def superuser_required(function):
     @wraps(function)
     def wrap(request, *args, **kwargs):
+        """
+        A wrap to prevent standard users from accessing the page. Code for
+        returning the permission denied exception is from
+        https://docs.djangoproject.com/en/3.2/ref/views/#the-403-http-forbidden-view
+        """
         if not request.user.is_superuser:
-            return HttpResponse(status=403)
+            raise PermissionDenied
         return function(request, *args, **kwargs)
     return wrap
